f43a1f7141ed30b8468fa38cf97f974f53c95279
Vini Fraga <vinifragam@gmail.com>
2021-02-02 23:50:47 -0300

    01: Aula 5

--- reactjs/v1/01-github-explorer/webpack.config.js
+++ reactjs/v1/01-github-explorer/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -10,6 +12,11 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
   module: {
     rules: [
       {
