2e56a9f509385462a87d7c877ac7b4d4539b2f5a
Vini Fraga <vinifragam@gmail.com>
2021-02-05 22:49:52 -0300

    01: Aula 20

--- reactjs/v1/01-github-explorer/src/components/RepositoryList.tsx
+++ reactjs/v1/01-github-explorer/src/components/RepositoryList.tsx
@@ -3,8 +3,14 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
