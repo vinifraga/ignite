7267e9b1c6f22db7990c1fee316c970e0888e9dc
Vini Fraga <vinifragam@gmail.com>
2021-02-05 21:12:46 -0300

    01: Aula 16

--- reactjs/v1/01-github-explorer/src/App.jsx
+++ reactjs/v1/01-github-explorer/src/App.jsx
@@ -1,13 +1,9 @@
 import './styles/global.scss';
 
 import { RepositoryList } from './components/RepositoryList';
-import { Counter } from './components/Counter';
 
 export function App() {
   return (
-    <>
-      <RepositoryList />
-      <Counter />
-    </>
+    <RepositoryList />
   )
 }
