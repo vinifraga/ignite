f935422b910bc20574a81c9f8f38b712d9c75004
Vini Fraga <vinifragam@gmail.com>
2021-02-17 22:06:48 -0300

    fix 08: summary outcome spacing

--- reactjs/v1/02-dtmoney/src/components/Summary/index.tsx
+++ reactjs/v1/02-dtmoney/src/components/Summary/index.tsx
@@ -20,7 +20,7 @@ export function Summary() {
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saídas"/>
         </header>
-        <strong>-R$500,00</strong>
+        <strong>- R$500,00</strong>
       </div>
 
       <div className="highlight-background">
