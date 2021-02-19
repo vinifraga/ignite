50c3949b00cf93000e8e28ec7d120a3a627f5795
Vini Fraga <vinifragam@gmail.com>
2021-02-19 00:13:27 -0300

    19: Cores dos botões

--- reactjs/v1/02-dtmoney/src/components/NewTransactionModal/index.tsx
+++ reactjs/v1/02-dtmoney/src/components/NewTransactionModal/index.tsx
@@ -47,6 +47,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
             type="button"
             onClick={() => { setType('deposit') }}
             isActive={type === 'deposit'}
+            activeColor="green"
           >
             <img src={incomeImg} alt="Entrada"/>
             <span>Entrada</span>
@@ -56,6 +57,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
             type="button"
             onClick={() => { setType('withdraw') }}
             isActive={type === 'withdraw'}
+            activeColor="red"
           >
             <img src={outcomeImg} alt="Saída"/>
             <span>Saída</span>
