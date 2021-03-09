ad8ccbfcd21a5d4b139e7966fc02d339aee29b73
Vini Fraga <vinifragam@gmail.com>
2021-03-08 23:37:27 -0300

    N07: Utilizando os métodos HTTP

--- node/v1/01-fundamentos-nodejs/README.md
+++ node/v1/01-fundamentos-nodejs/README.md
@@ -3,3 +3,4 @@
 ## Commits
 
 - **[05: Criando a estrutura do projeto](https://github.com/vinifraga/ignite/commit/973ba67bfc17912587faba70944154602e1660d3)**
+- **[06: Adicionando o Nodemon na aplicação](https://github.com/vinifraga/ignite/commit/0d2c5de043bdb07a1cce7bf152389d903bd2732c)**
--- node/v1/01-fundamentos-nodejs/src/index.js
+++ node/v1/01-fundamentos-nodejs/src/index.js
@@ -2,10 +2,24 @@ const express = require('express');
 
 const app = express();
 
-// localhost:3333
+app.get('/courses', (request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+})
 
-app.get('/', (request, response) => {
-  return response.json({ message: "Hello World Ignite! " })
+app.post('/courses', (request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+})
+
+app.put('/courses/:id', (request, response) => {
+  return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+})
+
+app.patch('/courses/:id', (request, response) => {
+  return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+})
+
+app.delete('/courses/:id', (request, response) => {
+  return response.json(["Curso 6", "Curso 2", "Curso 4"]);
 })
 
 app.listen(3333);
