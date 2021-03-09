3c95d3ab09cea9e77fd150bc332c330c3ab480f4
Vini Fraga <vinifragam@gmail.com>
2021-03-08 23:54:05 -0300

    N09: Tipos de parâmetros

--- node/v1/01-fundamentos-nodejs/README.md
+++ node/v1/01-fundamentos-nodejs/README.md
@@ -5,3 +5,4 @@
 - **[05: Criando a estrutura do projeto](https://github.com/vinifraga/ignite/commit/973ba67bfc17912587faba70944154602e1660d3)**
 - **[06: Adicionando o Nodemon na aplicação](https://github.com/vinifraga/ignite/commit/0d2c5de043bdb07a1cce7bf152389d903bd2732c)**
 - **[07: Utilizando os métodos HTTP](https://github.com/vinifraga/ignite/commit/ad8ccbfcd21a5d4b139e7966fc02d339aee29b73)**
+- **[08: Utilizando o insomnia](https://github.com/vinifraga/ignite/commit/c69e70b4afdf82d9b9c8ae80b6394bafb36b7988)**
--- node/v1/01-fundamentos-nodejs/src/index.js
+++ node/v1/01-fundamentos-nodejs/src/index.js
@@ -2,15 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/courses', (request, response) => {
+  const query = request.query;
+  console.log(query);
   return response.json(["Curso 1", "Curso 2", "Curso 3"]);
 })
 
 app.post('/courses', (request, response) => {
+  const body = request.body;
+  console.log(body);
   return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 })
 
 app.put('/courses/:id', (request, response) => {
+  const { id } = request.params;
+  console.log(id);
   return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
 })
 
