4d495b3f6b0a88ec65dc799303a78f8e36ac9c48
Vini Fraga <vinifragam@gmail.com>
2021-03-12 17:25:31 -0300

    E14: Introdução ao módulo Enum

--- elixir/v1/sum_list/lib/sum_list.ex
+++ elixir/v1/sum_list/lib/sum_list.ex
@@ -1,6 +1,8 @@
 defmodule SumList do
   def call(list), do: sum(list, 0)
 
+  def call_enum(list), do: Enum.any?(list, fn elem -> elem > 5 end)
+
   defp sum([], acc), do: acc
 
   defp sum([head | tail], acc) do
