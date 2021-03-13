247bee13023812cb2d035c024df1866037698d34
Vini Fraga <vinifragam@gmail.com>
2021-03-12 21:25:00 -0300

    E05: Lendo o arquivo em um stream

--- elixir/v1/reports_generator/README.md
+++ elixir/v1/reports_generator/README.md
@@ -3,3 +3,4 @@
 ## Commits
 - **[02: Criando nosso projeto e configurando o Credo](https://github.com/vinifraga/ignite/commit/177c8ad7495c4dcc6eac9f7532c1a4421f9b6ac4)**
 - **[03: Lendo nosso arquivo](https://github.com/vinifraga/ignite/commit/928f1273619ac5849c7876617e3a14036e25b269)**
+- **[04: Conhecendo o pipe operator](https://github.com/vinifraga/ignite/commit/cd3af2f1a4918951c84b89dc20de16a158fb86de)**
--- elixir/v1/reports_generator/lib/reports_generator.ex
+++ elixir/v1/reports_generator/lib/reports_generator.ex
@@ -1,10 +1,14 @@
 defmodule ReportsGenerator do
   def build(filename) do
     "reports/#{filename}"
-    |> File.read()
-    |> handle_file()
+    |> File.stream!()
+    |> Enum.map(fn line -> parse_line(line) end)
   end
 
-  defp handle_file({:ok, file_content}), do: file_content
-  defp handle_file({:error, _reason}), do: "Error while opening file!"
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
+  end
 end
