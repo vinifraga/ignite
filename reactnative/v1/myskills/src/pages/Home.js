9b36a4493835651f6712933a8c0bc539da790459
Vini Fraga <vinifragam@gmail.com>
2021-05-07 12:04:40 -0300

    RN08: Estrutura de um arquivo JSX

---
+++ reactnative/v1/myskills/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export function Home() {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <Text>React Native</Text>
+
+      <Text>Ignite</Text>
+    </View>
+  )
+}
