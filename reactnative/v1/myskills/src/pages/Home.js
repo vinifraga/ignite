be207181e2ee17c8f511c05864cb29afe59eed77
Vini Fraga <vinifragam@gmail.com>
2021-05-07 17:41:03 -0300

    RN09: Refatorando a estilização com o StyleSheet

--- reactnative/v1/myskills/src/pages/Home.js
+++ reactnative/v1/myskills/src/pages/Home.js
@@ -1,16 +1,61 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  TouchableOpacity
+} from 'react-native';
 
 export function Home() {
   return (
-    <View style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
-      <Text>React Native</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Rodrigo</Text>
 
-      <Text>Ignite</Text>
+      <TextInput style={styles.input} placeholder="New skill" placeholderTextColor="#555" />
+
+      <TouchableOpacity style={styles.button} activeOpacity={.7}>
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+
+      <Text style={[styles.title, { marginTop: 50 }]}>
+        My Skills
+      </Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingVertical: 70,
+    paddingHorizontal: 30
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7
+  },
+  button: {
+    backgroundColor: '#a370f7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold'
+  }
+})
