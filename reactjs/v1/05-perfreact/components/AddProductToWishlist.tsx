1af9744611f8cb5ca3525c23ab33870cfe7ccc92
Vini Fraga <vinifragam@gmail.com>
2021-05-10 17:24:59 -0300

    08: Dynamic import (Code Splitting)

---
+++ reactjs/v1/05-perfreact/components/AddProductToWishlist.tsx
@@ -0,0 +1,17 @@
+export interface AddProductToWishListProps {
+  onAddToWishList: () => void;
+  onRequestClose: () => void;
+}
+
+export function AddProductToWishList({
+  onAddToWishList,
+  onRequestClose
+}: AddProductToWishListProps) {
+  return (
+    <span>
+      Deseja adicionar aos favoritos?
+      <button onClick={onAddToWishList}>Sim</button>
+      <button onClick={onRequestClose}>Não</button>
+    </span>
+  )
+}
--- reactjs/v1/05-perfreact/components/ProductItem.tsx
+++ reactjs/v1/05-perfreact/components/ProductItem.tsx
@@ -1,4 +1,13 @@
-import { memo } from "react"
+import { memo, useState } from "react"
+import dynamic from 'next/dynamic';
+
+import { AddProductToWishListProps } from "./AddProductToWishlist"
+
+const AddProductToWishList = dynamic<AddProductToWishListProps>(() => {
+  return import ('./AddProductToWishlist').then(mod => mod.AddProductToWishList)
+}, {
+  loading: () => <span>Carregando...</span>
+})
 
 interface ProductItemProps {
   product: {
@@ -11,10 +20,19 @@ interface ProductItemProps {
 }
 
 export function ProductItemComponent({ product, onAddToWishList }: ProductItemProps) {
+  const [isAddingToWishList, setIsAddingToWishList] = useState(false);
+
   return (
     <div>
       {product.title} - <strong>{product.priceFormatted}</strong>
-      <button onClick={() => onAddToWishList(product.id)}>Add to wishlist</button>
+      <button onClick={() => setIsAddingToWishList(true)}>Adicionar aos favoritos</button>
+
+      { isAddingToWishList && (
+        <AddProductToWishList 
+          onAddToWishList={() => onAddToWishList(product.id)}
+          onRequestClose={() => setIsAddingToWishList(false)}
+        />
+      )}
     </div>
   )
 }
