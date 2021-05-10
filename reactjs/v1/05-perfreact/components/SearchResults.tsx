b86c177724767bcc018109822946d6ee0fc7adf3
Vini Fraga <vinifragam@gmail.com>
2021-05-10 17:38:12 -0300

    09: Virtualização

--- reactjs/v1/05-perfreact/components/SearchResults.tsx
+++ reactjs/v1/05-perfreact/components/SearchResults.tsx
@@ -1,4 +1,5 @@
-import { useMemo } from "react"
+import { List, ListRowRenderer } from 'react-virtualized';
+
 import { ProductItem } from "./ProductItem"
 
 interface SearchResultsProps {
@@ -13,19 +14,29 @@ interface SearchResultsProps {
 } 
 
 export function SearchResults({ results, totalPrice, onAddToWishList }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    )
+  }
+  
   return (
     <div>
       <h2>{totalPrice}</h2>
 
-      {results.map(product => {
-        return (
-          <ProductItem
-            key={product.id}
-            product={product}
-            onAddToWishList={onAddToWishList}
-          />
-        )
-      })}
+      <List 
+        height={300}
+        rowHeight={25}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   )
 }
