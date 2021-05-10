14d4f325d8bae56138b22dcd8f13e9f3ca90cc43
Vini Fraga <vinifragam@gmail.com>
2021-05-10 16:33:17 -0300

    05: useMemo

--- reactjs/v1/05-perfreact/components/SearchResults.tsx
+++ reactjs/v1/05-perfreact/components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react"
 import { ProductItem } from "./ProductItem"
 
 interface SearchResultsProps {
@@ -9,8 +10,16 @@ interface SearchResultsProps {
 } 
 
 export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price
+  }, 0)
+}, [results])
+
   return (
     <div>
+      <h2>{totalPrice}</h2>
+
       {results.map(product => {
         return (
           <ProductItem product={product} />
