4e076f9aaef41125625593f3745e929edae7395f
Vini Fraga <vinifragam@gmail.com>
2021-05-14 18:31:02 -0300

    R11: Componente: SignInButton

--- reactjs/v1/03-ignews/src/components/Header/index.tsx
+++ reactjs/v1/03-ignews/src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { SignInButton } from '../SignInButton';
+
 import styles from './styles.module.scss';
 
 export function Header() {
@@ -10,6 +12,8 @@ export function Header() {
           <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   )
