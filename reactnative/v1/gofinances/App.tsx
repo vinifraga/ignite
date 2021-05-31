ba27f22d46edc8b80ea070343eda6d147af3a5f6
Vini Fraga <vinifragam@gmail.com>
2021-05-31 13:00:28 -0300

    RN8: Criando estilos globais

--- reactnative/v1/gofinances/App.tsx
+++ reactnative/v1/gofinances/App.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
 import { Dashboard} from './src/screens/Dashboard';
+import theme from './src/global/styles/theme';
 
 export default function App() {
-  return <Dashboard />
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  )
 }
