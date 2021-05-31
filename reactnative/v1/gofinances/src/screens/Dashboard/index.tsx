a461a8a10460c554911ddc97d5c4a2a029abeb5a
Vini Fraga <vinifragam@gmail.com>
2021-05-31 14:20:53 -0300

    RN11: Utilizando proporções em medidas

--- reactnative/v1/gofinances/src/screens/Dashboard/index.tsx
+++ reactnative/v1/gofinances/src/screens/Dashboard/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import { 
+  Container, 
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName
+ } from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://github.com/vinifraga.png' }} />
+
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Vinícius</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
