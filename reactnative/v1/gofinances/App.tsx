3576fd89eaffa62e1e4f0846b2ea57a43c1c7b57
Vini Fraga <vinifragam@gmail.com>
2021-05-31 13:10:05 -0300

    RN9: Utilizando Fontes Personalizadas

--- reactnative/v1/gofinances/App.tsx
+++ reactnative/v1/gofinances/App.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
 
-import { Dashboard} from './src/screens/Dashboard';
+import { Dashboard } from './src/screens/Dashboard';
+
 import theme from './src/global/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
--- reactnative/v1/gofinances/src/global/styles/theme.ts
+++ reactnative/v1/gofinances/src/global/styles/theme.ts
@@ -15,5 +15,11 @@ export default {
     title: '#363f5f',
     text: '#969cb2',
     background: '#f0f2f5',
+  },
+
+  fonts: {
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold'
   }
 }
