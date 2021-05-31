4b545daf7b91a93cd6637b19119a19b2ae134027
Vini Fraga <vinifragam@gmail.com>
2021-05-31 14:33:10 -0300

    RN12: Utilizando Ícones

--- reactnative/v1/gofinances/src/screens/Dashboard/index.tsx
+++ reactnative/v1/gofinances/src/screens/Dashboard/index.tsx
@@ -8,7 +8,8 @@ import {
   Photo,
   User,
   UserGreeting,
-  UserName
+  UserName,
+  Icon
  } from './styles';
 
 export function Dashboard() {
@@ -24,6 +25,8 @@ export function Dashboard() {
               <UserName>Vinícius</UserName>
             </User>
           </UserInfo>
+
+          <Icon name="power" />
         </UserWrapper>
       </Header>
     </Container>
--- reactnative/v1/gofinances/src/screens/Dashboard/styles.ts
+++ reactnative/v1/gofinances/src/screens/Dashboard/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import { Feather } from '@expo/vector-icons';
 
 export const Container = styled.View`
   flex: 1;
@@ -20,6 +21,10 @@ export const Header = styled.View`
 export const UserWrapper = styled.View`
   width: 100%;
   padding: 0 24px;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const UserInfo = styled.View`
@@ -50,4 +55,9 @@ export const UserName = styled.Text`
 
   font-size: ${RFValue(18)}px;
   font-family: ${({ theme }) => theme.fonts.bold};
+`;
+
+export const Icon = styled(Feather)`
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: ${RFValue(24)}px;
 `;
