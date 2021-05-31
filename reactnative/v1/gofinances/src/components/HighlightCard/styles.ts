38cec541e585df6c3196ea289faa110d3bb68f5b
Vini Fraga <vinifragam@gmail.com>
2021-05-31 15:53:48 -0300

    RN14/15: Posicionar Cards, Tipar e acessar props

--- reactnative/v1/gofinances/src/components/HighlightCard/styles.ts
+++ reactnative/v1/gofinances/src/components/HighlightCard/styles.ts
@@ -1,9 +1,17 @@
-import styled from 'styled-components/native';
+import styled, {
+  css
+} from 'styled-components/native';
 import { Feather } from '@expo/vector-icons';
 import { RFValue } from 'react-native-responsive-fontsize';
 
-export const Container = styled.View`
-  background-color: ${({ theme }) => theme.colors.shape};
+interface TypeProps {
+  type: 'up' | 'down' | 'total';
+}
+
+export const Container = styled.View<TypeProps>`
+  background-color: ${({ theme, type }) => 
+    type === "total" ? theme.colors.secondary : theme.colors.shape
+  };
 
   width: ${RFValue(300)}px;
   border-radius: 5px;
@@ -11,8 +19,6 @@ export const Container = styled.View`
   padding: 19px 23px;
   padding-bottom: ${RFValue(42)}px;
   margin-right: 16px;
-
-  height: 300px;
 `;
 
 export const Header = styled.View`
@@ -20,30 +26,48 @@ export const Header = styled.View`
   justify-content: space-between;
 `;
 
-export const Title = styled.Text`
+export const Title = styled.Text<TypeProps>`
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${RFValue(14)}px;
 
-  color: ${({ theme }) => theme.colors.text_dark};
+  color: ${({ theme, type }) => 
+    type === 'total' ? theme.colors.shape : theme.colors.text_dark
+  };
 `;
 
-export const Icon = styled(Feather)`
+export const Icon = styled(Feather)<TypeProps>`
   font-size: ${RFValue(40)}px;
+
+  ${({ type }) => type === 'up' && css`
+    color: ${({ theme}) => theme.colors.success}
+  `}
+
+  ${({ type }) => type === 'down' && css`
+    color: ${({ theme}) => theme.colors.attention}
+  `}
+
+  ${({ type }) => type === 'total' && css`
+    color: ${({ theme}) => theme.colors.shape}
+  `}
 `;
 
 export const Footer = styled.View``;
 
-export const Amount = styled.Text`
+export const Amount = styled.Text<TypeProps>`
   font-family: ${({ theme }) => theme.fonts.medium};
   font-size: ${RFValue(32)}px;
 
-  color: ${({ theme }) => theme.colors.text_dark};
+  color: ${({ theme, type }) => 
+    type === 'total' ? theme.colors.shape : theme.colors.text_dark
+  };
   margin-top: 38px;
 `;
 
-export const LastTransaction = styled.Text`
+export const LastTransaction = styled.Text<TypeProps>`
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${RFValue(12)}px;
 
-  color: ${({ theme }) => theme.colors.text};
+  color: ${({ theme, type }) => 
+    type === 'total' ? theme.colors.shape : theme.colors.text
+  };
 `;
--- reactnative/v1/gofinances/src/screens/Dashboard/index.tsx
+++ reactnative/v1/gofinances/src/screens/Dashboard/index.tsx
@@ -34,9 +34,24 @@ export function Dashboard() {
       </Header>
     
       <HighlightCards>
-        <HighlightCard />
-        <HighlightCard />
-        <HighlightCard />
+        <HighlightCard 
+          title="Entradas" 
+          amount="R$ 17.400,00" 
+          lastTransaction="Última entrada dia 13 de abril"
+          type="up"
+        />
+        <HighlightCard 
+          title="Saídas" 
+          amount="R$ 1.259,00" 
+          lastTransaction="Última saída dia 03 de abril"
+          type="down"
+        />
+        <HighlightCard 
+          title="Total" 
+          amount="R$ 16.141,00" 
+          lastTransaction="01 à 16 de abril"
+          type="total"
+        />
       </HighlightCards>
     </Container>
   )
