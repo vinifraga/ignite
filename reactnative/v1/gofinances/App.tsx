5f441ce1e0dbe846d70f4d1fe623b6309edfa344
Vini Fraga <vinifragam@gmail.com>
2021-06-01 14:21:35 -0300

    RN02: Componente de Input

--- reactnative/v1/gofinances/App.tsx
+++ reactnative/v1/gofinances/App.tsx
@@ -8,7 +8,7 @@ import {
   Poppins_700Bold
 } from '@expo-google-fonts/poppins';
 
-import { Dashboard } from './src/screens/Dashboard';
+import { Register } from './src/screens/Register';
 
 import theme from './src/global/styles/theme';
 
@@ -25,7 +25,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   )
 }
