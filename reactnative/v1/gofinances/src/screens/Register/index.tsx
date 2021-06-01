7ae527d1cd8d91f870461bd4ae05bc08047fbaaf
Vini Fraga <vinifragam@gmail.com>
2021-06-01 15:50:40 -0300

    RN05: Componente de Select

--- reactnative/v1/gofinances/src/screens/Register/index.tsx
+++ reactnative/v1/gofinances/src/screens/Register/index.tsx
@@ -1,6 +1,7 @@
 
 import React, { useState } from 'react';
 import { Button } from '../../components/Form/Button';
+import { CategorySelect } from '../../components/Form/CategorySelect';
 
 import { Input } from '../../components/Form/Input';
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
@@ -50,6 +51,10 @@ export function Register() {
               isActive={transactionType === 'down'}
             />
           </TransactionTypes>
+
+          <CategorySelect 
+            title="Categoria"
+          />
         </Fields>
 
         <Button title="Enviar" />
