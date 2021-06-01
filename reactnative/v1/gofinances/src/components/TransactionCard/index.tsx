cc63fea4866ca40b6481c874b36b19765c1c1f64
Vini Fraga <vinifragam@gmail.com>
2021-06-01 10:58:17 -0300

    RN19: Deixando os cartões de transações dinâmicos

--- reactnative/v1/gofinances/src/components/TransactionCard/index.tsx
+++ reactnative/v1/gofinances/src/components/TransactionCard/index.tsx
@@ -17,6 +17,7 @@ interface Category {
 }
 
 interface Data {
+  type: 'positive' | 'negative'
   title: string;
   amount: string;
   category: Category;
@@ -32,11 +33,14 @@ export function TransactionCard({ data }: Props) {
     <Container>
       <Title>{data.title}</Title>
 
-      <Amount>{data.amount}</Amount>
+      <Amount type={data.type}>
+        {data.type === 'negative' && '- '}
+        {data.amount}
+      </Amount>
 
       <Footer>
         <Category>
-          <Icon name="dollar-sign" />
+          <Icon name={data.category.icon} />
           <CategoryName>{data.category.name}</CategoryName>
         </Category>
         <Date>{data.date}</Date>
--- reactnative/v1/gofinances/src/components/TransactionCard/styles.ts
+++ reactnative/v1/gofinances/src/components/TransactionCard/styles.ts
@@ -2,6 +2,10 @@ import styled from 'styled-components/native';
 import { Feather } from '@expo/vector-icons'
 import { RFValue } from 'react-native-responsive-fontsize';
 
+interface TransactionProps {
+  type: 'positive' | 'negative';
+}
+
 export const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.shape};
   border-radius: 5px;
@@ -14,10 +18,14 @@ export const Title = styled.Text`
   font-size: ${RFValue(14)}px;
 `;
 
-export const Amount = styled.Text`
+export const Amount = styled.Text<TransactionProps>`
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${RFValue(20)}px;
   margin-top: 2px;
+
+  color: ${({ theme, type }) => 
+    type === 'positive' ? theme.colors.success : theme.colors.attention
+  };
 `;
 
 export const Footer = styled.View`
