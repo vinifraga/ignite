4317eb4fb09c37fa22af3bcb044fd656dcc67187
Vini Fraga <vinifragam@gmail.com>
2021-06-15 14:54:44 -0300

    RN12: Estilizando a Tab Navigation

--- reactnative/v1/gofinances/src/routes/app.routes.tsx
+++ reactnative/v1/gofinances/src/routes/app.routes.tsx
@@ -1,22 +1,69 @@
 import React from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useTheme } from 'styled-components';
 
 import { Dashboard } from '../screens/Dashboard';
 import { Register } from '../screens/Register';
+import { Platform } from 'react-native';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppRoutes() {
+  const theme = useTheme()
+
   return (
-    <Navigator>
+    <Navigator
+      tabBarOptions={{
+        activeTintColor: theme.colors.secondary,
+        inactiveTintColor: theme.colors.text,
+        labelPosition: 'beside-icon',
+        style: {
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+          height: 88
+        }
+      }}
+    >
       <Screen 
         name="Listagem"
         component={Dashboard}
+        options={{
+          tabBarIcon: (({ size, color }) => (
+            <MaterialIcons 
+              name="format-list-bulleted"
+              size={size}
+              color={color}
+            />
+          ))
+        }}
       />
 
       <Screen 
         name="Cadastrar"
         component={Register}
+        options={{
+          tabBarIcon: (({ size, color }) => (
+            <MaterialIcons 
+              name="attach-money"
+              size={size}
+              color={color}
+            />
+          ))
+        }}
+      />
+
+<     Screen 
+        name="Resumo"
+        component={Register}
+        options={{
+          tabBarIcon: (({ size, color }) => (
+            <MaterialIcons 
+              name="pie-chart"
+              size={size}
+              color={color}
+            />
+          ))
+        }}
       />
     </Navigator>
   )
