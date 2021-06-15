4e6d6bed4a128b225239c5f52ca74adcf1415cd8
Vini Fraga <vinifragam@gmail.com>
2021-06-15 14:43:20 -0300

    RN11: Utilizando a Tab Navigation

--- reactnative/v1/gofinances/App.tsx
+++ reactnative/v1/gofinances/App.tsx
@@ -9,11 +9,11 @@ import {
   Poppins_500Medium,
   Poppins_700Bold
 } from '@expo-google-fonts/poppins';
+import { NavigationContainer } from '@react-navigation/native'
 
-import { CategorySelect } from './src/screens/CategorySelect';
+import { AppRoutes } from './src/routes/app.routes';
 
 import theme from './src/global/styles/theme';
-import { Register } from './src/screens/Register';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -28,7 +28,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   )
 }
