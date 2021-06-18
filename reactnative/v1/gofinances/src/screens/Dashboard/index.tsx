3b58eab560234be238a3a408fc37605c620c067e
Vinícius Fraga <vinifragam@gmail.com>
2021-06-18 14:00:59 -0300

    RN15: Finalizando a nossa Aplicação

--- reactnative/v1/gofinances/src/screens/Dashboard/index.tsx
+++ reactnative/v1/gofinances/src/screens/Dashboard/index.tsx
@@ -54,9 +54,15 @@ export function Dashboard() {
       type: 'positive' | 'negative'
     ) {
 
+    const collectionFiltered = collection
+    .filter((transaction) => transaction.type === type)
+
+    if (collectionFiltered.length === 0) {
+      return 0;
+    }
+
     const lastTransaction = new Date(Math.max.apply(
-      Math, collection
-        .filter((transaction) => transaction.type === type)
+      Math, collectionFiltered
         .map((transaction) => new Date(transaction.date).getTime())  
       )
     )
@@ -105,7 +111,9 @@ export function Dashboard() {
     const lastTransactionEntries = getLastTransactionDate(transactions, 'positive')
     const lastTransactionExpensives = getLastTransactionDate(transactions, 'negative')
     
-    const totalInterval = `01 a ${lastTransactionExpensives}`;
+    const totalInterval = lastTransactionExpensives === 0 
+      ? 'Não há transações' 
+      : `01 a ${lastTransactionExpensives}`;
 
     const total = entriesTotal - expensiveTotal;
 
@@ -115,14 +123,18 @@ export function Dashboard() {
           style: 'currency',
           currency: 'BRL'
         }),
-        lastTransaction: `Última entrada dia ${lastTransactionEntries}`
+        lastTransaction: lastTransactionEntries === 0 
+        ? 'Não há entradas' 
+        : `Última entrada dia ${lastTransactionEntries}` 
       },
       expensives: {
         amount: Number(expensiveTotal).toLocaleString('pt-BR', {
           style: 'currency',
           currency: 'BRL'
         }),
-        lastTransaction: `Última saída dia ${lastTransactionExpensives}`
+        lastTransaction: lastTransactionExpensives === 0 
+        ? 'Não há saídas' 
+        : `Última saída dia ${lastTransactionExpensives}`
       },
        total: {
         amount: Number(total).toLocaleString('pt-BR', {
