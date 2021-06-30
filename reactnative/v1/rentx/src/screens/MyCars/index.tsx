fe39ae31d5cca54da7657d8990f13d25c4aa2f05
Vinícius Fraga <vinifragam@gmail.com>
2021-06-30 15:57:41 -0300

    RN28: Listando Agendamentos do Usuário

--- reactnative/v1/rentx/src/screens/MyCars/index.tsx
+++ reactnative/v1/rentx/src/screens/MyCars/index.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { CarDTO } from '../../dtos/CarDTO';
+import { api } from '../../services/api';
 
 import {
   Container
 } from './styles';
 
 export function MyCars() {
+  const [cars, setCars] = useState<CarDTO[]>([])
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchCars() {
+      try {
+        const response = await api.get('schedules_byuser?user_id=1');
+        setCars(response.data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchCars();
+  }, [])
+
   return (
     <Container>
-
+      
     </Container>
   );
 }
--- reactnative/v1/rentx/src/screens/SchedulingDetails/index.tsx
+++ reactnative/v1/rentx/src/screens/SchedulingDetails/index.tsx
@@ -71,6 +71,11 @@ export function SchedulingDetails() {
         ...dates
       ]
 
+      await api.post(`schedules_byuser`, {
+        user_id: 1,
+        car
+      })
+
       await api.put(`schedules_bycars/${car.id}`, {
         id: car.id,
         unavailable_dates
