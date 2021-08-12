63549be526d8f33efa3b7368491382640a5e286b
Vini Fraga <vinifragam@gmail.com>
2021-08-12 00:42:21 -0300

    RN04: Instalação Reanimated

--- reactnative/v1/rentx/src/routes/stack.routes.tsx
+++ reactnative/v1/rentx/src/routes/stack.routes.tsx
@@ -7,12 +7,18 @@ import { Scheduling } from '../screens/Scheduling';
 import { SchedulingDetails } from '../screens/SchedulingDetails';
 import { SchedulingComplete } from '../screens/SchedulingComplete';
 import { MyCars } from '../screens/MyCars';
+import { Splash } from '../screens/Splash';
 
 const { Navigator, Screen } = createStackNavigator();
 
 export function StackRoutes() {
   return (
-    <Navigator headerMode="none">
+    <Navigator headerMode="none" initialRouteName="Splash">
+      <Screen 
+        name="Splash"
+        component={Splash}
+      />
+
       <Screen 
         name="Home"
         component={Home}
---
+++ reactnative/v1/rentx/src/screens/Splash/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {
+  Container
+} from './styles';
+
+export function Splash() {
+  return (
+    <Container>
+
+    </Container>
+  );
+}
