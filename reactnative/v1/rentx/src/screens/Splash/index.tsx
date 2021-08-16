c7abfd74bd9c09fce965b2ecfed1098a9cf3bed7
Vini Fraga <vinifragam@gmail.com>
2021-08-16 19:14:23 -0300

    RN06: Primeira Animação

--- reactnative/v1/rentx/src/screens/Splash/index.tsx
+++ reactnative/v1/rentx/src/screens/Splash/index.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
+import { Button, StyleSheet } from 'react-native';
+import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated';
 
 import {
   Container
 } from './styles';
 
 export function Splash() {
+  const animation = useSharedValue(0);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: animation.value }
+      ]
+    }
+  })
+
+  function handleAnimationPosition() {
+    animation.value = Math.random() * 100;
+  }
+
   return (
     <Container>
+      <Animated.View style={[styles.box, animatedStyles]} />
 
+      <Button title="Mover" onPress={handleAnimationPosition} />
     </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red'
+  }
+})
