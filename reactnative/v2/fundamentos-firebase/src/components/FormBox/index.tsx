2dcf76e4034b1864ef71a9546cd51df1da3d65d6
Vini Fraga <vinifragam@gmail.com>
2022-01-04 17:35:17 -0300

    FF14: Adicionando via código documentos no Firestore

--- reactnative/v2/fundamentos-firebase/src/components/FormBox/index.tsx
+++ reactnative/v2/fundamentos-firebase/src/components/FormBox/index.tsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import firestore from '@react-native-firebase/firestore';
 
 import { Container } from './styles';
 import { ButtonIcon } from '../ButtonIcon';
 import { Input } from '../Input';
+import { Alert } from 'react-native';
 
 export function FormBox() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(0);
+
+  async function handleProductAdd() {
+    try {
+      const response = await firestore()
+      .collection('products')
+      .add(
+        { 
+          description, quantity, done: false 
+        }
+      )
+
+      Alert.alert('Produto adicionado com sucesso')
+    } catch (error) {
+      console.log(error)
+    }
+    
+  }
+
   return (
     <Container>
       <Input
         placeholder="Nome do produto"
         size="medium"
+        onChangeText={setDescription}
       />
 
       <Input
@@ -17,12 +40,13 @@ export function FormBox() {
         keyboardType="numeric"
         size="small"
         style={{ marginHorizontal: 8 }}
+        onChangeText={value => setQuantity(Number(value))}
       />
 
       <ButtonIcon
         size='large'
         icon="add-shopping-cart"
-        onPress={() => { }}
+        onPress={handleProductAdd}
       />
     </Container>
   );
