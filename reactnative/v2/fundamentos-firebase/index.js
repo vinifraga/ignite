e38c2af766fb4ccddf088ad49a46cc44c08d3bf6
Vini Fraga <vinifragam@gmail.com>
2022-01-04 23:22:31 -0300

    FF28: Instalação e Configuração do Emulador do Cloud Firestore

--- reactnative/v2/fundamentos-firebase/index.js
+++ reactnative/v2/fundamentos-firebase/index.js
@@ -1,4 +1,7 @@
 import 'react-native-gesture-handler';
+import '@react-native-firebase/app';
+
+import firestore from '@react-native-firebase/firestore';
 import { registerRootComponent } from 'expo';
 
 import App from './App';
@@ -7,3 +10,13 @@ import App from './App';
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+
+// set the host and the port property to connect to the emulator
+// set these before any read/write operations occur to ensure it doesn't affect your Cloud Firestore data!
+if (__DEV__) {
+  firestore().terminate();
+  firestore().clearPersistence();
+  firestore().useEmulator('localhost', 8080);
+}
+
+firestore();
