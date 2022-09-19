ada333fd2d26916067422834c8351f7fee692b73
Vini Fraga <vinifragam@gmail.com>
2022-09-19 17:13:23 -0300

    CSS-in-JS - Listando os filtros

--- reactnative/v3/igniteteams/src/screens/Players/index.tsx
+++ reactnative/v3/igniteteams/src/screens/Players/index.tsx
@@ -3,10 +3,15 @@ import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { useState } from "react";
+import { FlatList } from "react-native";
 
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 
 export function Players() {
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -27,9 +32,24 @@ export function Players() {
         />
       </Form>
 
-      <Filter 
-        title="Time A"
-      />
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter 
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumbersOfPlayers>
+          {players.length}
+        </NumbersOfPlayers>
+      </HeaderList>
     </Container>
   )
 }
