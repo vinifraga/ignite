0406b7fc5af95cb98d9b6e78e27cc0f6a0164214
Vini Fraga <vinifragam@gmail.com>
2022-09-19 17:45:49 -0300

    CSS-in-JS - Criando o Componente PlayerCard

--- reactnative/v3/igniteteams/src/screens/Players/index.tsx
+++ reactnative/v3/igniteteams/src/screens/Players/index.tsx
@@ -3,6 +3,7 @@ import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { PlayerCard } from "@components/PlayerCard";
 import { useState } from "react";
 import { FlatList } from "react-native";
 
@@ -10,7 +11,7 @@ import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 
 export function Players() {
   const [team, setTeam] = useState('Time A');
-  const [players, setPlayers] = useState([]);
+  const [players, setPlayers] = useState(['Rodrigo', 'Vini']);
 
   return (
     <Container>
@@ -50,6 +51,17 @@ export function Players() {
           {players.length}
         </NumbersOfPlayers>
       </HeaderList>
+
+      <FlatList 
+        data={players}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <PlayerCard 
+            name={item}
+            onRemove={() => { }}
+          />
+        )}
+      />
     </Container>
   )
 }
