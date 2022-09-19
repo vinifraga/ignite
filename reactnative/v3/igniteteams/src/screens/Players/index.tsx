3c9134a62a59e7b5368b4d36acee047c3974a106
Vini Fraga <vinifragam@gmail.com>
2022-09-19 17:49:57 -0300

    CSS-in-JS - Finalizando a lista de Players

--- reactnative/v3/igniteteams/src/screens/Players/index.tsx
+++ reactnative/v3/igniteteams/src/screens/Players/index.tsx
@@ -1,17 +1,20 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { ListEmpty } from "@components/ListEmpty";
 import { PlayerCard } from "@components/PlayerCard";
-import { useState } from "react";
-import { FlatList } from "react-native";
+import { Button } from "@components/Button";
 
 import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 
 export function Players() {
   const [team, setTeam] = useState('Time A');
-  const [players, setPlayers] = useState(['Rodrigo', 'Vini']);
+  const [players, setPlayers] = useState([]);
 
   return (
     <Container>
@@ -61,6 +64,18 @@ export function Players() {
             onRemove={() => { }}
           />
         )}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message="Não há pessoas nesse time."
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[{ paddingBottom: 100 },players.length === 0 && { flex: 1 }]}
+      />
+
+      <Button 
+        title="Remover Turma"
+        type="SECONDARY"
       />
     </Container>
   )
