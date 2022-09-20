7906185f037c367d8f47569e70cedec77075da40
Vini Fraga <vinifragam@gmail.com>
2022-09-20 10:55:10 -0300

    Navegação - Estrutura de Navegação

--- reactnative/v3/igniteteams/src/routes/app.routes.tsx
+++ reactnative/v3/igniteteams/src/routes/app.routes.tsx
@@ -9,7 +9,7 @@ const { Navigator, Screen } = createNativeStackNavigator();
 export function AppRoutes() {
 
   return (
-    <Navigator>
+    <Navigator screenOptions={{ headerShown: false }}>
       <Screen 
         name="groups" 
         component={Groups} 
