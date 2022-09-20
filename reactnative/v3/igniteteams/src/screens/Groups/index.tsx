368256e36441ba5968fbc4c17c4dfbf647ef0624
Vini Fraga <vinifragam@gmail.com>
2022-09-20 20:10:27 -0300

    Navegação - Implementando o voltar

--- reactnative/v3/igniteteams/src/screens/Groups/index.tsx
+++ reactnative/v3/igniteteams/src/screens/Groups/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
@@ -11,6 +12,12 @@ import { Container } from './styles';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
+  
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate('new');
+  }
 
   return (
     <Container>
@@ -36,6 +43,7 @@ export function Groups() {
 
       <Button 
         title='Criar nova turma'
+        onPress={handleNewGroup}
       />
     </Container>
   );
--- reactnative/v3/igniteteams/src/screens/NewGroup/index.tsx
+++ reactnative/v3/igniteteams/src/screens/NewGroup/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from "@react-navigation/native";
+
 import { Container, Content, Icon } from "./styles";
 
 import { Header } from "@components/Header";
@@ -6,6 +8,14 @@ import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 
 export function NewGroup() {
+  const navigation = useNavigation();
+
+  function handleNew() {
+    navigation.navigate('players', {
+      group: 'Rocket'
+    });
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -25,6 +35,7 @@ export function NewGroup() {
         <Button 
           title="Criar"
           style={{ marginTop: 20 }}
+          onPress={handleNew}
         />
       </Content>
     </Container>
