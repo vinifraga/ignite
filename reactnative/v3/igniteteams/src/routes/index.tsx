0290971b6987648acbc2a3d19f9b5357f69c8597
Vini Fraga <vinifragam@gmail.com>
2022-09-20 20:22:51 -0300

    Navegação - Prevenindo Glitch na navegação

--- reactnative/v3/igniteteams/src/routes/index.tsx
+++ reactnative/v3/igniteteams/src/routes/index.tsx
@@ -1,11 +1,17 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { View } from "react-native";
+import { useTheme } from "styled-components/native";
 
 import { AppRoutes } from "./app.routes";
 
 export function Routes() {
+  const { COLORS } = useTheme()
+
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <View style={{ flex: 1, backgroundColor: COLORS.GRAY_600 }}>
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </View>
   )
 }
