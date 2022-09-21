55a13fbc84b56a444547620a675d14655b69d500
Vini Fraga <vinifragam@gmail.com>
2022-09-21 00:12:48 -0300

    Local Storage - Tratando exceções

--- reactnative/v3/igniteteams/src/screens/NewGroup/index.tsx
+++ reactnative/v3/igniteteams/src/screens/NewGroup/index.tsx
@@ -9,6 +9,8 @@ import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 
 import { groupCreate } from "@storage/group/groupCreate";
+import { AppError } from "@utils/AppError";
+import { Alert } from "react-native";
 
 export function NewGroup() {
   const [group, setGroup] = useState('');
@@ -17,13 +19,22 @@ export function NewGroup() {
 
   async function handleNew() {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert('Novo Grupo', 'Informe o nome da turma.');
+      }
+
       await groupCreate(group);
     
       navigation.navigate('players', {
         group
       });
     } catch (error) {
-      console.log(error);
+      if (error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message);
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.');
+        console.log(error);
+      }
     }
   }
 
