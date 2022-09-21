cc3cfb81445a6d9c22237aa9f23e3a513d24fdb3
Vini Fraga <vinifragam@gmail.com>
2022-09-21 00:29:08 -0300

    Local Storage - Listando Players

--- reactnative/v3/igniteteams/src/storage/player/playerAddByGroup.ts
+++ reactnative/v3/igniteteams/src/storage/player/playerAddByGroup.ts
@@ -2,13 +2,23 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { PLAYER_COLLECTION } from "@storage/storageConfig";
 import { AppError } from "@utils/AppError";
+import { playersGetByGroup } from "./playersGetByGroup";
 
 import { PlayerStorageDTO } from "./PlayerStorageDTO";
 
 export async function playerAddByGroup(newPlayer: PlayerStorageDTO, group: string) {
   try {
-    
-    await AsyncStorage.setItem(`${PLAYER_COLLECTION}-${group}`, '');
+    const storedPlayers = await playersGetByGroup(group);
+
+    const playerAlreadyExists = storedPlayers.filter(player => player.name === newPlayer.name);
+
+    if (playerAlreadyExists.length > 0) {
+      throw new AppError('Essa pessoa já está adicionada em um time aqui.');
+    }
+
+    const storage = JSON.stringify([...storedPlayers, newPlayer]);
+
+    await AsyncStorage.setItem(`${PLAYER_COLLECTION}-${group}`, storage);
   } catch (error) {
     throw error;
   }
