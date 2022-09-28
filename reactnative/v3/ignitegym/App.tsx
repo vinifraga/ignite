0598d2f1450ceb52878ef46120d2088f2ff4ab59
Vinícius Fraga <vinifragam@gmail.com>
2022-09-28 10:30:06 -0300

    Estrutura da Aplicação - Personalizando a StatusBar

--- reactnative/v3/ignitegym/App.tsx
+++ reactnative/v3/ignitegym/App.tsx
@@ -1,10 +1,15 @@
-import { Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#202024' }}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Text>Hello World</Text> : <View />}
     </View>
   );
