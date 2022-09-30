3fd7ec53b6f5a9adee017ab062ae0ab1dd8ff1ec
Vini Fraga <vinifragam@gmail.com>
2022-09-30 12:22:12 -0300

    Component Library - Instalando o NativeBase

--- reactnative/v3/ignitegym/App.tsx
+++ reactnative/v3/ignitegym/App.tsx
@@ -1,16 +1,17 @@
-import { Text, View, StatusBar } from 'react-native';
+import { View, StatusBar } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#202024' }}>
+    <NativeBaseProvider>
       <StatusBar 
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Text>Hello World</Text> : <View />}
-    </View>
+      {fontsLoaded ? <View /> : <View />}
+    </NativeBaseProvider>
   );
 }
