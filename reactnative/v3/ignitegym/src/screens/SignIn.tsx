73acd0ecf1d9601766f609310a842d5e564d4f3d
Vini Fraga <vinifragam@gmail.com>
2022-09-30 13:49:31 -0300

    Component Library - Componente Heading

--- reactnative/v3/ignitegym/src/screens/SignIn.tsx
+++ reactnative/v3/ignitegym/src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { Center, Image, Text, VStack } from "native-base";
+import { Center, Heading, Image, Text, VStack } from "native-base";
 
 import LogoSvg from '@assets/logo.svg';
 import backgroundImg from '@assets/background.png';
@@ -18,6 +18,12 @@ export function SignIn() {
         <Text color="gray.100" fontSize="sm">
           Treine sua mente e o seu corpo
         </Text>
+      </Center>
+
+      <Center>
+        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+          Acesse sua conta
+        </Heading>
       </Center>
     </VStack>
   );
