dcc93443260ffdff4092efb1978a1e57e31fce80
Vini Fraga <vinifragam@gmail.com>
2022-09-30 12:39:02 -0300

    Component Library - Customizando o tema default

--- reactnative/v3/ignitegym/App.tsx
+++ reactnative/v3/ignitegym/App.tsx
@@ -1,18 +1,21 @@
 import { View, StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
 import { Loading } from '@components/Loading';
+import { THEME } from './src/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar 
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {!fontsLoaded ? <View /> : <Loading />}
+      {fontsLoaded ? <View /> : <Loading />}
     </NativeBaseProvider>
   );
 }
