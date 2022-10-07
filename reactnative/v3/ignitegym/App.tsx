2e11336805ddb4b2d30619f55864d89f3829e13a
Vini Fraga <vinifragam@gmail.com>
2022-10-07 11:32:28 -0300

    Component Library - Criando a SignUp

--- reactnative/v3/ignitegym/App.tsx
+++ reactnative/v3/ignitegym/App.tsx
@@ -4,7 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import { Loading } from '@components/Loading';
 import { THEME } from './src/theme';
-import { SignIn } from '@screens/SignIn';
+import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -16,7 +16,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <SignUp /> : <Loading />}
     </NativeBaseProvider>
   );
 }
