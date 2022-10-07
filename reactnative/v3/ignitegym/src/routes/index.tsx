9e5c0f65ad44379f9db5ffc30a60c14360fa5d14
Vini Fraga <vinifragam@gmail.com>
2022-10-07 17:14:20 -0300

    Rotas Privadas - Utilizando o Bottom Tab

--- reactnative/v3/ignitegym/src/routes/index.tsx
+++ reactnative/v3/ignitegym/src/routes/index.tsx
@@ -2,6 +2,7 @@ import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { Box, useTheme } from "native-base";
 
 import { AuthRoutes } from "./auth.routes";
+import { AppRoutes } from "./app.routes";
 
 export function Routes() {
   const { colors } = useTheme();
@@ -12,7 +13,7 @@ export function Routes() {
   return (
     <Box flex={1} bg="gray.700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   )
