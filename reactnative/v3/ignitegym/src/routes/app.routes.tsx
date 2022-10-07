88f3dd52db7fb9a4c8d23c7026c13a991276e239
Vini Fraga <vinifragam@gmail.com>
2022-10-07 17:17:18 -0300

    Rotas Privadas - Tipando as rotas do App

--- reactnative/v3/ignitegym/src/routes/app.routes.tsx
+++ reactnative/v3/ignitegym/src/routes/app.routes.tsx
@@ -1,11 +1,20 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator, BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 
 import { Home } from '@screens/Home';
 import { History } from '@screens/History';
 import { Profile } from '@screens/Profile';
 import { Exercise } from '@screens/Exercise';
 
-const { Navigator, Screen } = createBottomTabNavigator();
+type AppRoutes = {
+  home: undefined;
+  history: undefined;
+  profile: undefined;
+  exercise: undefined;
+}
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
   return (
