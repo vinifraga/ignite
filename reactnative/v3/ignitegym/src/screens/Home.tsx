3fb70f8b2bbf2408f68c9cc1b57060ef1762489a
Vinícius Fraga <vinifragam@gmail.com>
2022-10-13 19:00:00 -0300

    Finalizando as Interfaces - Exibindo o grupo selecionado

--- reactnative/v3/ignitegym/src/screens/Home.tsx
+++ reactnative/v3/ignitegym/src/screens/Home.tsx
@@ -2,15 +2,26 @@ import { HStack, VStack } from "native-base";
 
 import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
+import { useState } from "react";
 
 export function Home() {
-  return (
+  const [groupSelected, setGroupSelected] = useState('costa');
+
+    return (
     <VStack flex={1}>
       <HomeHeader />
-      
+
       <HStack>
-        <Group name="Costa" />
-        <Group name="Ombro" />
+        <Group 
+          name="Costa" 
+          isActive={groupSelected === 'costa'}
+          onPress={() => setGroupSelected('costa')} 
+        />
+        <Group 
+          name="Ombro" 
+          isActive={groupSelected === 'ombro'} 
+          onPress={() => setGroupSelected('ombro')} 
+        />
       </HStack>
     </VStack>
   )
