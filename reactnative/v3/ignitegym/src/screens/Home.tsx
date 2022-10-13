b96753ae0eebe61629aec6c1514609cf2d42d315
Vinícius Fraga <vinifragam@gmail.com>
2022-10-13 19:09:10 -0300

    Finalizando as Interfaces - Header da lista de exercícios

--- reactnative/v3/ignitegym/src/screens/Home.tsx
+++ reactnative/v3/ignitegym/src/screens/Home.tsx
@@ -1,8 +1,8 @@
-import { FlatList, VStack } from "native-base";
+import { useState } from "react";
+import { FlatList, Heading, HStack, Text, VStack } from "native-base";
 
 import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
-import { useState } from "react";
 
 export function Home() {
   const [groups, setGroups] = useState(['costas', 'bíceps', 'tríceps', 'ombro']);
@@ -32,6 +32,16 @@ export function Home() {
         my={10}
         maxHeight={10}
       />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">4</Text>
+        </HStack>
+      </VStack>
     </VStack>
   )
 }
