c2636356dd05a9369593a9a6bdc1e56b34c03241
Vinícius Fraga <vinifragam@gmail.com>
2022-10-13 18:38:34 -0300

    Finalizando as Interfaces - Componente UserPhoto

---
+++ reactnative/v3/ignitegym/src/components/Avatar.tsx
@@ -0,0 +1,11 @@
+import { IImageProps, Image } from "native-base";
+
+type Props = IImageProps & {
+  size: number;
+}
+
+export function UserPhoto({ size, ...rest }: Props) {
+  return (
+    <Image w={size} h={size} rounded="full" borderWidth={2} borderColor="gray.400" {...rest} />
+  )
+}
--- reactnative/v3/ignitegym/src/components/HomeHeader.tsx
+++ reactnative/v3/ignitegym/src/components/HomeHeader.tsx
@@ -1,8 +1,16 @@
 import { Avatar, Heading, HStack, Text, VStack } from "native-base";
+import { UserPhoto } from "./Avatar";
 
 export function HomeHeader() {
   return (
     <HStack bgColor="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto 
+        source={{ uri: 'https://github.com/rodrigorgtic.png' }}
+        alt="Imagem do usuário"
+        size={16}
+        mr={4} 
+      />
+      
       <VStack>
         <Text color="gray.100" fontSize="md">
           Olá,
