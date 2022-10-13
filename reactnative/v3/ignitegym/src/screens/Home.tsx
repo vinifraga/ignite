3ebe06b8019fd6545df7653e5fcf66f8f51a252f
Vinícius Fraga <vinifragam@gmail.com>
2022-10-13 19:05:40 -0300

    Finalizando as Interfaces - Listando os grupos

--- reactnative/v3/ignitegym/src/screens/Home.tsx
+++ reactnative/v3/ignitegym/src/screens/Home.tsx
@@ -1,28 +1,37 @@
-import { HStack, VStack } from "native-base";
+import { FlatList, VStack } from "native-base";
 
 import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
 import { useState } from "react";
 
 export function Home() {
+  const [groups, setGroups] = useState(['costas', 'bíceps', 'tríceps', 'ombro']);
   const [groupSelected, setGroupSelected] = useState('costa');
 
     return (
     <VStack flex={1}>
       <HomeHeader />
 
-      <HStack>
-        <Group 
-          name="Costa" 
-          isActive={groupSelected === 'costa'}
-          onPress={() => setGroupSelected('costa')} 
-        />
-        <Group 
-          name="Ombro" 
-          isActive={groupSelected === 'ombro'} 
-          onPress={() => setGroupSelected('ombro')} 
-        />
-      </HStack>
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => {
+          return (
+            <Group 
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)} 
+            />
+            )
+          }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 8
+        }}
+        my={10}
+        maxHeight={10}
+      />
     </VStack>
   )
 }
