cfdad2a3d76fff87d11fbf108e41f87a3e03948c
Vinícius Fraga <vinifragam@gmail.com>
2022-10-13 18:45:29 -0300

    Finalizando as Interfaces - Botão Logout

--- reactnative/v3/ignitegym/src/components/HomeHeader.tsx
+++ reactnative/v3/ignitegym/src/components/HomeHeader.tsx
@@ -1,4 +1,7 @@
-import { Avatar, Heading, HStack, Text, VStack } from "native-base";
+import { TouchableOpacity } from "react-native";
+import { Heading, HStack, Icon, Text, VStack } from "native-base";
+import { MaterialIcons } from '@expo/vector-icons';
+
 import { UserPhoto } from "./Avatar";
 
 export function HomeHeader() {
@@ -20,6 +23,15 @@ export function HomeHeader() {
           Rodrigo
         </Heading>
       </VStack>
+
+      <TouchableOpacity>
+        <Icon
+          as={MaterialIcons}
+          name="logout" 
+          color="gray.200" 
+          size={7}
+        />
+      </TouchableOpacity>
     </HStack>
   )
 }
