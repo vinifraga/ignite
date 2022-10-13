378ad68a5c48cb391c9cb3208b3f0c8167dfd3e7
Vinícius Fraga <vinifragam@gmail.com>
2022-10-13 18:30:20 -0300

    Finalizando as Interfaces - Posicionando na vertical e horizontal

--- reactnative/v3/ignitegym/src/screens/Home.tsx
+++ reactnative/v3/ignitegym/src/screens/Home.tsx
@@ -1,9 +1,11 @@
-import { Center, Text } from "native-base";
+import { Center, Text, VStack } from "native-base";
+
+import { HomeHeader } from "@components/HomeHeader";
 
 export function Home() {
   return (
-    <Center flex={1}>
-      <Text color="white">Home</Text>
-    </Center>
+    <VStack flex={1}>
+      <HomeHeader />
+    </VStack>
   )
 }
