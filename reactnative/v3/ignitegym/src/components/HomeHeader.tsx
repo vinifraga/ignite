49399c4b652ae035e52cddab49cb70f3a784f33b
Vinícius Fraga <vinifragam@gmail.com>
2022-10-13 18:32:07 -0300

    Finalizando as Interfaces - Estilizando o HomeHeader

--- reactnative/v3/ignitegym/src/components/HomeHeader.tsx
+++ reactnative/v3/ignitegym/src/components/HomeHeader.tsx
@@ -2,13 +2,13 @@ import { Avatar, Heading, HStack, Text, VStack } from "native-base";
 
 export function HomeHeader() {
   return (
-    <HStack>
+    <HStack bgColor="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <VStack>
-        <Text color="gray.100">
+        <Text color="gray.100" fontSize="md">
           Olá,
         </Text>
 
-        <Heading color="gray.100">
+        <Heading color="gray.100" fontSize="md">
           Rodrigo
         </Heading>
       </VStack>
