f4f7f894687dfc6ef303cfde187d074173c0cc8e
Vinícius Fraga <vinifragam@gmail.com>
2022-10-13 19:43:01 -0300

    Finalizando as Interfaces - Componente HistoryCard

--- reactnative/v3/ignitegym/src/screens/History.tsx
+++ reactnative/v3/ignitegym/src/screens/History.tsx
@@ -1,11 +1,16 @@
 import { VStack } from "native-base";
 
 import { ScreenHeader } from "@components/ScreenHeader";
+import { HistoryCard } from "@components/HistoryCard";
 
 export function History() {
   return (
     <VStack>
       <ScreenHeader title="Histórico de Exercícios" />
+
+      <HistoryCard />
+      <HistoryCard />
+      <HistoryCard />
     </VStack>
   )
 }
