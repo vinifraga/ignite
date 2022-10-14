3b422f266ace2fd690e174f87692983f6fdb4617
Vini Fraga <vinifragam@gmail.com>
2022-10-14 18:24:42 -0300

    Finalizando as interfaces - Imagem do Exercício

--- reactnative/v3/ignitegym/src/screens/Exercise.tsx
+++ reactnative/v3/ignitegym/src/screens/Exercise.tsx
@@ -1,4 +1,4 @@
-import { Heading, HStack, Icon, Text, VStack } from "native-base";
+import { Heading, HStack, Icon, Image, Text, VStack } from "native-base";
 import { TouchableOpacity } from "react-native";
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from "@react-navigation/native";
@@ -39,6 +39,18 @@ export function Exercise() {
             </Text>
           </HStack>
         </HStack>
+      </VStack>
+
+      <VStack p={8}>
+        <Image 
+          w="full"
+          h={80}
+          source={{ uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg' }}
+          alt="Nome do exercício"
+          mb={3}
+          resizeMode="cover"
+          rounded="lg"
+        />
       </VStack>
     </VStack>
   )
