2134ada8c6180e6f8d878fd498052a6aaded302d
Vini Fraga <vinifragam@gmail.com>
2022-10-14 19:58:12 -0300

    Formulários - Obtendo a mensagem de erro

--- reactnative/v3/ignitegym/src/screens/SignUp.tsx
+++ reactnative/v3/ignitegym/src/screens/SignUp.tsx
@@ -19,7 +19,7 @@ type FormDataProps = {
 export function SignUp() {
   const navigation = useNavigation();
 
-  const { control, handleSubmit } = useForm<FormDataProps>();
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>();
 
   function handleGoBack() {
     navigation.goBack();
@@ -70,6 +70,11 @@ export function SignUp() {
               />
             )}
           />
+
+          <Text color="white">
+            {errors.name?.message}
+          </Text>
+
           <Controller
             name="email"
             control={control}
