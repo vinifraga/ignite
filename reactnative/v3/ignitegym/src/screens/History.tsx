0e63864bafb85916fca047c499225b9cd9ffe9b2
Vini Fraga <vinifragam@gmail.com>
2022-10-14 15:21:05 -0300

    Finalizando as interfaces - Utilizando SectionList

--- reactnative/v3/ignitegym/src/screens/History.tsx
+++ reactnative/v3/ignitegym/src/screens/History.tsx
@@ -1,16 +1,38 @@
-import { VStack } from "native-base";
+import { useState } from "react";
+import { Heading, VStack, SectionList } from "native-base";
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { HistoryCard } from "@components/HistoryCard";
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: '26.08.22',
+      data: ['Puxada frontal', 'Remada unilateral']
+    },
+    {
+      title: '27.08.22',
+      data: ['Puxada frontal']
+    }
+  ]);
+
   return (
     <VStack>
       <ScreenHeader title="Histórico de Exercícios" />
 
-      <HistoryCard />
-      <HistoryCard />
-      <HistoryCard />
+      <SectionList 
+        sections={exercises}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <HistoryCard />
+        )}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+      />
     </VStack>
   )
 }
