e6cb856547b936e7ce6fc12d3a751b161477a7b5
Vini Fraga <vinifragam@gmail.com>
2022-10-14 19:29:59 -0300

    Galeria de Fotos - Utilizando mensagens toast

--- reactnative/v3/ignitegym/src/screens/Profile.tsx
+++ reactnative/v3/ignitegym/src/screens/Profile.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { Center, Heading, ScrollView, Skeleton, Text, VStack } from "native-base";
-import { Alert, TouchableOpacity } from "react-native";
+import { Center, Heading, ScrollView, Skeleton, Text, useToast, VStack } from "native-base";
+import { TouchableOpacity } from "react-native";
 import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
 
@@ -14,6 +14,8 @@ const PHOTO_SIZE = 33;
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
   const [userPhoto, setUserPhoto] = useState('https://github.com/rodrigorgtic.png');
+
+  const toast = useToast();
 
   async function handleUserPhotoSelect() {
     setPhotoIsLoading(true);
@@ -34,7 +36,11 @@ export function Profile() {
         const photoInfo = await FileSystem.getInfoAsync(photoSelected.uri);
 
         if (photoInfo.size && (photoInfo.size / 1024 / 1024) > 5) {
-          return Alert.alert('Essa imagem é muito grande. Escolha uma de até 5MB.');
+          return toast.show({
+            title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+            placement: 'top',
+            bgColor: 'red.500',
+          })
         }
 
         setUserPhoto(photoSelected.uri);
