409cf1d2cb606183531ca1c32b20b46dcb5f6cf2
Vini Fraga <vinifragam@gmail.com>
2022-10-17 18:03:22 -0300

    Contextos no React Native - Exibindo nome do usuário

--- reactnative/v3/ignitegym/mobile/src/components/HomeHeader.tsx
+++ reactnative/v3/ignitegym/mobile/src/components/HomeHeader.tsx
@@ -2,9 +2,13 @@ import { TouchableOpacity } from "react-native";
 import { Heading, HStack, Icon, Text, VStack } from "native-base";
 import { MaterialIcons } from '@expo/vector-icons';
 
+import { useAuth } from "@hooks/useAuth";
+
 import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
+  const { user } = useAuth();
+
   return (
     <HStack bgColor="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <UserPhoto 
@@ -20,7 +24,7 @@ export function HomeHeader() {
         </Text>
 
         <Heading color="gray.100" fontSize="md" fontFamily="heading">
-          Rodrigo
+          {user.name}
         </Heading>
       </VStack>
 
