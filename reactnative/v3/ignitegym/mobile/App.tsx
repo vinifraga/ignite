fc9f7ca7ffbc124206c626b727adb0caa989ceb0
Vini Fraga <vinifragam@gmail.com>
2022-10-17 16:16:43 -0300

    Contextos no React Native - Unificando o create e provider context

--- reactnative/v3/ignitegym/mobile/App.tsx
+++ reactnative/v3/ignitegym/mobile/App.tsx
@@ -5,7 +5,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 import { Loading } from '@components/Loading';
 import { THEME } from './src/theme';
 import { Routes } from '@routes/index';
-import { AuthContext } from '@contexts/AuthContext';
+import { AuthContext, AuthContextProvider } from '@contexts/AuthContext';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -25,7 +25,9 @@ export default function App() {
           avatar: 'rodrigo.png'
         }
       }}>
-        {fontsLoaded ? <Routes /> : <Loading />}
+        <AuthContextProvider>
+          {fontsLoaded ? <Routes /> : <Loading />}
+        </AuthContextProvider>
       </AuthContext.Provider>
     </NativeBaseProvider>
   );
