7f79d635ec83d8a54a2815efdb652ec02c258328
Vini Fraga <vinifragam@gmail.com>
2022-10-17 16:12:42 -0300

    Contextos no React Native - Tipando contexto

--- reactnative/v3/ignitegym/mobile/App.tsx
+++ reactnative/v3/ignitegym/mobile/App.tsx
@@ -18,10 +18,12 @@ export default function App() {
         translucent
       />
       <AuthContext.Provider value={{
-        id: '1',
-        name: 'Rodrigo',
-        email: 'rodrigo@email.com',
-        avatar: 'rodrigo.png'
+        user: {
+          id: '1',
+          name: 'Rodrigo',
+          email: 'rodrigo@email.com',
+          avatar: 'rodrigo.png'
+        }
       }}>
         {fontsLoaded ? <Routes /> : <Loading />}
       </AuthContext.Provider>
