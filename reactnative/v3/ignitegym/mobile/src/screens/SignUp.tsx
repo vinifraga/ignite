6cfeff33953a82572b1bd75e12a0253a91554757
Vini Fraga <vinifragam@gmail.com>
2022-10-17 12:20:55 -0300

    HTTP Client - Utilizando Axios

--- reactnative/v3/ignitegym/mobile/src/screens/SignUp.tsx
+++ reactnative/v3/ignitegym/mobile/src/screens/SignUp.tsx
@@ -5,6 +5,8 @@ import { useForm, Controller } from "react-hook-form";
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup'
 
+import { api } from "@services/api";
+
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
@@ -37,17 +39,13 @@ export function SignUp() {
   }
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
-    const response = await fetch('http://192.168.15.6:3333/users', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ name, email, password })
-    })
-    
-    const data = await response.json();
-    console.log(data);
+    const response = await api.post('/users', {
+      name,
+      email,
+      password
+    });
+
+    console.log(response.data);
   }
 
   return (
