799207880301ddf2aa3d6525b1661b42fc880bf8
Vini Fraga <vinifragam@gmail.com>
2022-10-17 18:02:17 -0300

    Contextos no React Native - Exibindo loading enquanto carrega usuário

--- reactnative/v3/ignitegym/mobile/src/contexts/AuthContext.tsx
+++ reactnative/v3/ignitegym/mobile/src/contexts/AuthContext.tsx
@@ -8,6 +8,7 @@ import { storageUserGet, storageUserSave } from "@storage/storageUser";
 export type AuthContextDataProps = {
   user: UserDTO;
   signIn: (email: string, password: string) => Promise<void>;
+  isLoadingUserStoragedData: boolean;
 }
 
 type AuthContextProviderProps = {
@@ -18,6 +19,7 @@ export const AuthContext = createContext<AuthContextDataProps>({} as AuthContext
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [user, setUser] = useState<UserDTO>({} as UserDTO);
+  const [isLoadingUserStoragedData, setIsLoadingUserStoragedData] = useState(true);
 
   async function signIn(email: string, password: string) {
     try {
@@ -33,10 +35,16 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   }
 
   async function loadUserData() {
-    const userLogged = await storageUserGet();
+    try {
+      const userLogged = await storageUserGet();
 
-    if (userLogged) {
-      setUser(userLogged);
+      if (userLogged) {
+        setUser(userLogged);
+      }
+    } catch (error) {
+      throw error;
+    } finally {
+      setIsLoadingUserStoragedData(false);
     }
   }
 
@@ -45,7 +53,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   }, [])
   
   return (
-    <AuthContext.Provider value={{ user, signIn }}>
+    <AuthContext.Provider value={{ user, signIn, isLoadingUserStoragedData }}>
       {children}
     </AuthContext.Provider>
   )
--- reactnative/v3/ignitegym/mobile/src/routes/index.tsx
+++ reactnative/v3/ignitegym/mobile/src/routes/index.tsx
@@ -5,13 +5,19 @@ import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
 import { useAuth } from "@hooks/useAuth";
 
+import { Loading } from "@components/Loading";
+
 export function Routes() {
   const { colors } = useTheme();
-  const { user } = useAuth();
+  const { user, isLoadingUserStoragedData } = useAuth();
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
   
+  if (isLoadingUserStoragedData) {
+    return <Loading />
+  }
+
   return (
     <Box flex={1} bg="gray.700">
       <NavigationContainer theme={theme}>
