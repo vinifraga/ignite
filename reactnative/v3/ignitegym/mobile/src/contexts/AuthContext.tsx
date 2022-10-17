412145c37b8fcdd5b3dbcb0251f24738456c4c8b
Vini Fraga <vinifragam@gmail.com>
2022-10-17 17:57:49 -0300

    Contextos no React Native - Carregando usuário logado

--- reactnative/v3/ignitegym/mobile/src/contexts/AuthContext.tsx
+++ reactnative/v3/ignitegym/mobile/src/contexts/AuthContext.tsx
@@ -1,9 +1,9 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 
 import { UserDTO } from "@dtos/UserDTO";
 
 import { api } from "@services/api";
-import { storageUserSave } from "@storage/storageUser";
+import { storageUserGet, storageUserSave } from "@storage/storageUser";
 
 export type AuthContextDataProps = {
   user: UserDTO;
@@ -31,6 +31,18 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       throw error;
     }
   }
+
+  async function loadUserData() {
+    const userLogged = await storageUserGet();
+
+    if (userLogged) {
+      setUser(userLogged);
+    }
+  }
+
+  useEffect(() => {
+    loadUserData()
+  }, [])
   
   return (
     <AuthContext.Provider value={{ user, signIn }}>
