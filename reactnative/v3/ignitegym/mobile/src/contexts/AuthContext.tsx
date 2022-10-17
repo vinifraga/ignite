849a3c61bbd47dc58f14fa02dd12be2d514cdbbf
Vini Fraga <vinifragam@gmail.com>
2022-10-17 17:52:29 -0300

    Contextos no React Native - Persistindo usuário logado

--- reactnative/v3/ignitegym/mobile/src/contexts/AuthContext.tsx
+++ reactnative/v3/ignitegym/mobile/src/contexts/AuthContext.tsx
@@ -3,6 +3,7 @@ import { createContext, ReactNode, useState } from "react";
 import { UserDTO } from "@dtos/UserDTO";
 
 import { api } from "@services/api";
+import { storageUserSave } from "@storage/storageUser";
 
 export type AuthContextDataProps = {
   user: UserDTO;
@@ -24,6 +25,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     
       if (data.user) {
         setUser(data.user);
+        storageUserSave(data.user);
       }
     } catch (error) {
       throw error;
