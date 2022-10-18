a34af6ebe123abcda67cad34828a8945c7211e1c
Vini Fraga <vinifragam@gmail.com>
2022-10-18 15:45:49 -0300

    Autenticação JWT - Recuperando o token do storage

--- reactnative/v3/ignitegym/mobile/src/contexts/AuthContext.tsx
+++ reactnative/v3/ignitegym/mobile/src/contexts/AuthContext.tsx
@@ -4,7 +4,7 @@ import { UserDTO } from "@dtos/UserDTO";
 
 import { api } from "@services/api";
 import { storageUserGet, storageUserRemove, storageUserSave } from "@storage/storageUser";
-import { storageAuthTokenSave } from "@storage/storageAuthToken";
+import { storageAuthTokenGet, storageAuthTokenSave } from "@storage/storageAuthToken";
 
 export type AuthContextDataProps = {
   user: UserDTO;
@@ -23,15 +23,18 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [user, setUser] = useState<UserDTO>({} as UserDTO);
   const [isLoadingUserStoragedData, setIsLoadingUserStoragedData] = useState(true);
 
-  async function storageUserAndToken(userData: UserDTO, token: string) {
+  function userAndTokenUpdate(userData: UserDTO, token: string) {
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  
+    setUser(userData);
+  }
+
+  async function storageUserAndTokenSave(userData: UserDTO, token: string) {
     try {
       setIsLoadingUserStoragedData(true);
 
-      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    
       await storageUserSave(userData);
       await storageAuthTokenSave(token);
-      setUser(userData);
     } catch (error) {
       throw error;
     } finally {
@@ -44,10 +47,13 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       const { data } = await api.post('/sessions', { email, password });
     
       if (data.user && data.token) {
-        storageUserAndToken(data.user, data.token);
+        await storageUserAndTokenSave(data.user, data.token);
+        userAndTokenUpdate(data.user, data.token);
       }
     } catch (error) {
       throw error;
+    } finally {
+      setIsLoadingUserStoragedData(false);
     }
   }
 
@@ -65,10 +71,13 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
   async function loadUserData() {
     try {
-      const userLogged = await storageUserGet();
+      setIsLoadingUserStoragedData(true);
 
-      if (userLogged) {
-        setUser(userLogged);
+      const userLogged = await storageUserGet();
+      const token = await storageAuthTokenGet();
+
+      if (token && userLogged) {
+        userAndTokenUpdate(userLogged, token);
       }
     } catch (error) {
       throw error;
