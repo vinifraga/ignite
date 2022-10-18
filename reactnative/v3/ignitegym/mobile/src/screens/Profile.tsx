ee7a76ddeea12680bff712d5b7c25e102dad66fc
Vini Fraga <vinifragam@gmail.com>
2022-10-18 17:21:47 -0300

    Consumindo o Backend - Preparando a validação do formulário

--- reactnative/v3/ignitegym/mobile/src/screens/Profile.tsx
+++ reactnative/v3/ignitegym/mobile/src/screens/Profile.tsx
@@ -4,6 +4,8 @@ import { TouchableOpacity } from "react-native";
 import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
 import { Controller, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup'
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
@@ -21,6 +23,10 @@ type FormDataProps = {
   confirm_password: string;
 }
 
+const profileSchema = yup.object({
+  name: yup.string().required('Informe o nome.'),
+})
+
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
   const [userPhoto, setUserPhoto] = useState('https://github.com/rodrigorgtic.png');
@@ -28,11 +34,12 @@ export function Profile() {
   const toast = useToast();
   const { user } = useAuth();
 
-  const { control, handleSubmit } = useForm<FormDataProps>({
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
     defaultValues: {
       name: user.name,
       email: user.email
-    }
+    },
+    resolver: yupResolver(profileSchema)
   });
 
   async function handleUserPhotoSelect() {
@@ -114,6 +121,7 @@ export function Profile() {
                 placeholder="Nome"
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.name?.message}
               />
             )}
           />
@@ -155,6 +163,7 @@ export function Profile() {
                 placeholder="Nova senha"
                 secureTextEntry
                 onChangeText={onChange}
+                errorMessage={errors.password?.message}
               />
             )}
           />
@@ -167,6 +176,7 @@ export function Profile() {
                 placeholder="Confirme a nova senha"
                 secureTextEntry
                 onChangeText={onChange}
+                errorMessage={errors.confirm_password?.message}
               />
             )}
           />
