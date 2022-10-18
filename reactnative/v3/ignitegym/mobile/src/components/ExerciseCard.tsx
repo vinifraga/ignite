14f122131accc5ae35e66f0db7345b96078209c3
Vini Fraga <vinifragam@gmail.com>
2022-10-18 16:16:58 -0300

    Consumindo o Backend - Carregando imagem

--- reactnative/v3/ignitegym/mobile/src/components/ExerciseCard.tsx
+++ reactnative/v3/ignitegym/mobile/src/components/ExerciseCard.tsx
@@ -3,6 +3,7 @@ import { TouchableOpacity, TouchableOpacityProps } from "react-native";
 import { Entypo } from '@expo/vector-icons';
 
 import { ExerciseDTO } from "@dtos/ExerciseDTO";
+import { api } from "@services/api";
 
 type Props = TouchableOpacityProps & {
   data: ExerciseDTO;
@@ -13,7 +14,7 @@ export function ExerciseCard({ data, ...rest }: Props) {
     <TouchableOpacity {...rest}>
       <HStack bg="gray.500" alignItems="center" p={2} pr={4} rounded="md" mb={3}>
         <Image 
-          source={{ uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg' }}
+          source={{ uri: `${api.defaults.baseURL}/exercise/thumb/${data.thumb}` }}
           alt="Imagem do exercício"
           w={16}
           h={16}
