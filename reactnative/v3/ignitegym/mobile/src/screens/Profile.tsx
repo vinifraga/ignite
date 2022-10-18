b37717b2f644884cd46533ba55da939a4373c25c
Vini Fraga <vinifragam@gmail.com>
2022-10-18 17:31:37 -0300

    Consumindo o Backend - Validando a senha

--- reactnative/v3/ignitegym/mobile/src/screens/Profile.tsx
+++ reactnative/v3/ignitegym/mobile/src/screens/Profile.tsx
@@ -25,6 +25,8 @@ type FormDataProps = {
 
 const profileSchema = yup.object({
   name: yup.string().required('Informe o nome.'),
+  password: yup.string().min(6, 'A senha deve ter pelo menos 6 dígitos').nullable().transform((value) => !!value ? value : null),
+  confirm_password: yup.string().oneOf([yup.ref('password'), null], 'A confirmação de senha não confere.').nullable().transform((value) => !!value ? value : null)
 })
 
 export function Profile() {
