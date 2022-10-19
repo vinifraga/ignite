503495101dc7cf91bb24cf00cd1078bc56adda72
Vini Fraga <vinifragam@gmail.com>
2022-10-18 21:14:47 -0300

    Consumindo o Backend - Atualizando usuário no backend

--- reactnative/v3/ignitegym/mobile/src/screens/Profile.tsx
+++ reactnative/v3/ignitegym/mobile/src/screens/Profile.tsx
@@ -12,6 +12,8 @@ import { UserPhoto } from "@components/UserPhoto";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 import { useAuth } from "@hooks/useAuth";
+import { api } from "@services/api";
+import { AppError } from "@utils/AppError";
 
 const PHOTO_SIZE = 33;
 
@@ -48,6 +50,7 @@ const profileSchema = yup.object({
 })
 
 export function Profile() {
+  const [isUpdating, setIsUpdating] = useState(false);
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
   const [userPhoto, setUserPhoto] = useState('https://github.com/rodrigorgtic.png');
 
@@ -98,7 +101,27 @@ export function Profile() {
   }
 
   async function handleProfileUpdate(data: FormDataProps) {
-    console.log(data);
+    try {
+      setIsUpdating(true);
+
+      await api.put('/users', data);
+      toast.show({
+        title: 'Perfil atualizado com sucesso!',
+        placement: 'top',
+        bgColor: 'green.500'
+      })
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError ? error.message : 'Não foi possível atualizar dados. Tente novamente mais tarde';
+      
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      }) 
+    } finally {
+      setIsUpdating(false);
+    }
   }
 
   return (
@@ -205,6 +228,7 @@ export function Profile() {
             title="Atualizar"
             onPress={handleSubmit(handleProfileUpdate)}
             mt={4}
+            isLoading={isUpdating}
           />
         </Center>
       </ScrollView>
