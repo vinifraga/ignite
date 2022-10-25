90c5ebbbb4e17ce485048f4d9b339ad5d47b0091
Vini Fraga <vinifragam@gmail.com>
2022-10-24 23:02:47 -0300

    Estrutura da aplicação - Styled Components

--- reactjs/2022/02-ignite-timer/src/App.tsx
+++ reactjs/2022/02-ignite-timer/src/App.tsx
@@ -1,5 +1,13 @@
+import { Button } from "./components/Button";
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button />
+    </>
   )
 }
