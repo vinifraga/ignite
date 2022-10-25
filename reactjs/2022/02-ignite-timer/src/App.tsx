ffc5ee84d942b062b4d3018284ab79a4f3ad807f
Vini Fraga <vinifragam@gmail.com>
2022-10-24 23:06:44 -0300

    Estrutura da aplicação - Configurando temas

--- reactjs/2022/02-ignite-timer/src/App.tsx
+++ reactjs/2022/02-ignite-timer/src/App.tsx
@@ -1,13 +1,17 @@
+import { ThemeProvider } from "styled-components";
+
 import { Button } from "./components/Button";
+
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="danger" />
       <Button variant="success" />
       <Button />
-    </>
+    </ThemeProvider>
   )
 }
