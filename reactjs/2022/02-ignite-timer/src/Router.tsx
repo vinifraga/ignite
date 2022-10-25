434a5d61ffbe3fbfa9a75f6c6f045425e55ed01c
Vini Fraga <vinifragam@gmail.com>
2022-10-24 23:36:16 -0300

    Páginas e rotas - Layout de rotas

--- reactjs/2022/02-ignite-timer/src/Router.tsx
+++ reactjs/2022/02-ignite-timer/src/Router.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
 
+import { DefaultLayout } from './layouts/DefaultLayout'
+
 import { History } from './pages/History'
 import { Home } from './pages/Home'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
