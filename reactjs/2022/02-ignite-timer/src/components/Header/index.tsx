38519e9a27e7734f15d1c159d6ec032157702416
Vini Fraga <vinifragam@gmail.com>
2022-10-24 23:55:05 -0300

    Páginas e rotas - Header & Layout

---
+++ reactjs/2022/02-ignite-timer/src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import { Scroll, Timer } from 'phosphor-react'
+
+import { HeaderContainer } from './styles'
+
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <NavLink to="/" end title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" end title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
