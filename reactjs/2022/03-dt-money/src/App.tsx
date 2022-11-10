1bc8c8dac86579feb8b79d2db96968bb82817319
Vini Fraga <vinifragam@gmail.com>
2022-11-10 20:56:59 -0300

    Estrutura visual - Setup do projeto

---
+++ reactjs/2022/03-dt-money/src/App.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "styled-components";
+
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <h1>Hello World</h1>
+    </ThemeProvider>
+  )
+}
