1b62e90b5887a2b44bcc5e536e6fee6785818bd0
Vini Fraga <vinifragam@gmail.com>
2022-11-10 21:33:46 -0300

    Estrutura visual - Componente: Header

--- reactjs/2022/03-dt-money/src/App.tsx
+++ reactjs/2022/03-dt-money/src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { Transactions } from "./pages/Transactions";
 
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
@@ -8,7 +9,7 @@ export function App() {
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
 
-      <h1>Hello World</h1>
+      <Transactions />
     </ThemeProvider>
   )
 }
