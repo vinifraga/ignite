9071537e9bec102c882e037b1063cf75c6b82fa2
Vini Fraga <vinifragam@gmail.com>
2022-11-10 21:54:30 -0300

    Estrutura visual - Componente: SearchForm

--- reactjs/2022/03-dt-money/src/pages/Transactions/index.tsx
+++ reactjs/2022/03-dt-money/src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SearchForm'
 import {
   PriceHighlight,
   TransactionsContainer,
@@ -13,6 +14,8 @@ export function Transactions() {
       <Summary />
 
       <TransactionsContainer>
+        <SearchForm />
+
         <TransactionsTable>
           <tbody>
             <tr>
