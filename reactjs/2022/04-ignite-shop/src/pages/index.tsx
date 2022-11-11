db996a072c766b7e36f13de1afe2936875734ed6
Vini Fraga <vinifragam@gmail.com>
2022-11-11 20:52:57 -0300

    Estrutura visual - Estrutura da Home

--- reactjs/2022/04-ignite-shop/src/pages/index.tsx
+++ reactjs/2022/04-ignite-shop/src/pages/index.tsx
@@ -1,11 +1,33 @@
+import Image from "next/image"
+
 import { styled } from "../styles"
 
-const Button = styled('button', {
-  backgroundColor: "$green300"
-})
+import { HomeContainer, Product } from "../styles/pages/home"
+
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
 
 export default function Home() {
   return (
-    <Button>Enviar</Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
