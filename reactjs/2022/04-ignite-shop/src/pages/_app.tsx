438d867ac003dc72ee78225b230a30f950622430
Vini Fraga <vinifragam@gmail.com>
2022-11-11 17:43:00 -0300

    Estrutura visual - Imagens no Next.js

--- reactjs/2022/04-ignite-shop/src/pages/_app.tsx
+++ reactjs/2022/04-ignite-shop/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
 
 import { globalStyles } from '../styles/global'
 
@@ -11,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
 
       <Component {...pageProps} />
