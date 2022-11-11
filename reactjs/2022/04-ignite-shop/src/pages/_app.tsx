720070daef5a002e9433e30506f84134e5baca61
Vini Fraga <vinifragam@gmail.com>
2022-11-11 17:32:41 -0300

    Estrutura visual - Cabeçalho da aplicação

--- reactjs/2022/04-ignite-shop/src/pages/_app.tsx
+++ reactjs/2022/04-ignite-shop/src/pages/_app.tsx
@@ -2,8 +2,19 @@ import type { AppProps } from 'next/app'
 
 import { globalStyles } from '../styles/global'
 
+import logoImg from '../assets/logo.svg';
+import { Container, Header } from '../styles/pages/app';
+
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
