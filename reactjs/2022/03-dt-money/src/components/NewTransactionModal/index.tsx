3cc397ce1c5c397eb37ad3c767bfc51ca40a7bcc
Vini Fraga <vinifragam@gmail.com>
2022-11-10 22:32:32 -0300

    Estrutura visual - Botões de entrada e saída

--- reactjs/2022/03-dt-money/src/components/NewTransactionModal/index.tsx
+++ reactjs/2022/03-dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,13 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { X } from 'phosphor-react'
+import { X, ArrowCircleDown, ArrowCircleUp } from 'phosphor-react'
 
-import { CloseButton, Content, Overlay } from './styles'
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from './styles'
 
 export function NewTransactionModal() {
   return (
@@ -19,6 +25,18 @@ export function NewTransactionModal() {
           <input type="text" placeholder="Descrição" required />
           <input type="text" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+
+            <TransactionTypeButton variant="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
 
           <button type="submit">Cadastrar</button>
         </form>
