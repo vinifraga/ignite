dde918313862969e32fa4340f0d63e836003dee5
Vini Fraga <vinifragam@gmail.com>
2022-11-11 17:22:50 -0300

    Estrutura visual - Configurando Stitches

--- reactjs/2022/04-ignite-shop/src/pages/index.tsx
+++ reactjs/2022/04-ignite-shop/src/pages/index.tsx
@@ -1,5 +1,11 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: "$rocketseat"
+})
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Button>Enviar</Button>
   )
 }
---
+++ reactjs/2022/04-ignite-shop/src/styles/index.ts
@@ -0,0 +1,9 @@
+import { createStitches } from "@stitches/react";
+
+export const { config, styled, globalCss, keyframes, getCssText, css, theme, createTheme } = createStitches({
+  theme: {
+    colors: {
+      rocketseat: '#8257e6'
+    }
+  }
+})
