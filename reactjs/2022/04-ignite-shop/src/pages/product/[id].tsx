c0bc5641d3d55e91d89b8206a8efcaf8e95adc7e
Vini Fraga <vinifragam@gmail.com>
2022-12-01 16:28:58 -0300

    Produto & Checkout - Estrutura da página de produto

--- reactjs/2022/04-ignite-shop/src/pages/product/[id].tsx
+++ reactjs/2022/04-ignite-shop/src/pages/product/[id].tsx
@@ -1,7 +1,30 @@
 import { useRouter } from 'next/router'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
 
 export default function Product() {
   const { query } = useRouter()
 
-  return <h1>Product: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Necessitatibus nam quidem nemo ducimus labore dicta quo aut veritatis
+          quibusdam magni tenetur quia aspernatur eum, beatae maiores vel ea
+          mollitia cupiditate.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
