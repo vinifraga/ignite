1b769d3026403ed418534ab78b4381a0b03fc93c
Vini Fraga <vinifragam@gmail.com>
2022-12-02 10:44:16 -0300

    Produto & Checkout - Dados da compra no sucesso

--- reactjs/2022/04-ignite-shop/src/pages/success.tsx
+++ reactjs/2022/04-ignite-shop/src/pages/success.tsx
@@ -1,20 +1,66 @@
+import { GetServerSideProps } from 'next'
+import Image from 'next/image'
 import Link from 'next/link'
+import Stripe from 'stripe'
+import invariant from 'tiny-invariant'
+import { stripe } from '../lib/stripe'
 import { ImageContainer, SuccessContainer } from '../styles/pages/success'
 
-export default function Success() {
+interface SuccessProps {
+  customerName: string
+  product: {
+    name: string
+    imageUrl: string
+  }
+}
+
+export default function Success({ customerName, product }: SuccessProps) {
   return (
     <SuccessContainer>
       <h1>Compra efetuada</h1>
 
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={120} height={110} alt="" />
+      </ImageContainer>
 
       <p>
-        Uhuul <strong>Vinícius Fraga</strong>, sua{' '}
-        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
-        casa.
+        Uhuul <strong>{customerName}</strong>, sua{' '}
+        <strong>{product.name}</strong> já está a caminho da sua casa.
       </p>
 
       <Link href="/">Voltar ao catálogo</Link>
     </SuccessContainer>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const { session_id: sessionId } = query
+
+  if (typeof sessionId !== 'string')
+    throw new Error('Invalid session_id value.')
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product'],
+  })
+
+  const customerName = session.customer_details?.name
+  const product = session.line_items?.data[0].price?.product
+
+  invariant(isStripeProduct(product), 'Product not found.')
+
+  return {
+    props: {
+      customerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0],
+      },
+    },
+  }
+}
+
+function isStripeProduct(
+  product: string | Stripe.Product | Stripe.DeletedProduct | undefined,
+): product is Stripe.Product {
+  return (product as Stripe.Product).active !== undefined
 }
