e3919dd447701ef992e05d2a438d79409065f39a
Vini Fraga <vinifragam@gmail.com>
2023-01-04 13:46:49 -0300

    onesignal android config

--- reactnative/2022/igniteshoes/App.tsx
+++ reactnative/2022/igniteshoes/App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
+import OneSignal from 'react-native-onesignal';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Routes } from './src/routes';
@@ -8,6 +9,8 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.setAppId('c5249431-8cc4-4795-a3ee-ec8c5578aa36')
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
