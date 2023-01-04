1830de17cb21e3068182abcb4f1816a48d87fad6
Vini Fraga <vinifragam@gmail.com>
2023-01-04 16:46:25 -0300

    Push Notifications - Utilizando tags para identificar usuário

--- reactnative/2022/igniteshoes/App.tsx
+++ reactnative/2022/igniteshoes/App.tsx
@@ -9,12 +9,15 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+import { tagUserEmailCreate } from './src/notifications/notificationsTags';
 
 OneSignal.setAppId('c5249431-8cc4-4795-a3ee-ec8c5578aa36')
 OneSignal.setEmail('vinifragam@gmail.com')
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  tagUserEmailCreate('vinifragam@gmail.com');
 
   return (
     <NativeBaseProvider theme={THEME}>
