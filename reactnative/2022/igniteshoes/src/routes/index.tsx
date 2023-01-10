b872ed1558531e10d26acf7a2aea1fb0f1d6321d
Vinícius Fraga <vinifragam@gmail.com>
2023-01-10 11:50:27 -0300

    Push Notifications - Refatorando o componente de Notificação

--- reactnative/2022/igniteshoes/src/routes/index.tsx
+++ reactnative/2022/igniteshoes/src/routes/index.tsx
@@ -2,16 +2,35 @@ import { useTheme } from 'native-base';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 
 import { AppRoutes } from './app.routes';
+import { useState, useEffect } from 'react';
+import OneSignal, { OSNotification, NotificationReceivedEvent } from 'react-native-onesignal';
+import { Notification } from '../components/Notification';
 
 export function Routes() {
+  const [notification, setNotification] = useState<OSNotification | null>()
+
   const { colors } = useTheme();
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
 
+  useEffect(() => {
+    const unsubscribe = OneSignal.setNotificationWillShowInForegroundHandler((notificationReceivedEvent: NotificationReceivedEvent) => {
+      const response = notificationReceivedEvent.getNotification()
+
+      setNotification(response)
+    })
+
+    return () => unsubscribe
+  })
+
   return (
     <NavigationContainer theme={theme}>
       <AppRoutes />
-    </NavigationContainer>
+
+      {/* {notification?.title && ( */}
+        <Notification title={'notification.title'} onClose={() => {setNotification(null)}} />
+      {/* )} */}
+    </NavigationContainer>  
   );
 }
