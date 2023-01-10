73c5a0bc4eca75e0a47e2a033f371255bbe4182b
Vinícius Fraga <vinifragam@gmail.com>
2023-01-10 16:02:43 -0300

    Deep Linking - Removendo item do carrinho

--- reactnative/2022/igniteshoes/src/contexts/CartContext.tsx
+++ reactnative/2022/igniteshoes/src/contexts/CartContext.tsx
@@ -37,6 +37,7 @@ export function CartContextProvider({ children }: CartContextProviderProps) {
     try {
       const response = await storageProductRemove(productId);
       setCart(response);
+      tagCartUpdate(response.length.toString())
     } catch (error) {
       throw error;
     }
