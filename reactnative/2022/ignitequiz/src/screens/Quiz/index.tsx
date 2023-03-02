8396222ceeba41ff43506ace652e8370741788d8
Vinícius Fraga <vinifragam@gmail.com>
2023-03-02 20:40:25 -0300

    Feedback táctil e sonoro - Tocando som

--- reactnative/2022/ignitequiz/src/screens/Quiz/index.tsx
+++ reactnative/2022/ignitequiz/src/screens/Quiz/index.tsx
@@ -11,6 +11,7 @@ import Animated, {
   withSequence,
   withTiming,
 } from "react-native-reanimated";
+import { Audio } from "expo-av";
 
 import { useNavigation, useRoute } from "@react-navigation/native";
 
@@ -58,6 +59,16 @@ export function Quiz() {
   const route = useRoute();
   const { id } = route.params as Params;
 
+  async function playSound(isCorrect: boolean) {
+    const file = isCorrect
+      ? require("../../assets/correct.mp3")
+      : require("../../assets/wrong.mp3");
+    const { sound } = await Audio.Sound.createAsync(file, { shouldPlay: true });
+
+    await sound.setPositionAsync(0);
+    await sound.playAsync();
+  }
+
   function handleSkipConfirm() {
     Alert.alert("Pular", "Deseja realmente pular a questão?", [
       { text: "Sim", onPress: () => handleNextQuestion() },
@@ -94,10 +105,12 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
+      playSound(true);
       setPoints((prevState) => prevState + 1);
       setStatusReply(1);
       handleNextQuestion();
     } else {
+      playSound(false);
       setStatusReply(2);
       shakeAnimation();
     }
