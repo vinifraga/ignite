92e584f136c2edefdebbe798e499bbeecdde0850
Vinícius Fraga <vinifragam@gmail.com>
2023-03-02 12:39:28 -0300

    Microinterações -  Swipeable Menu

--- reactnative/2022/ignitequiz/src/screens/History/index.tsx
+++ reactnative/2022/ignitequiz/src/screens/History/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { View, ScrollView, TouchableOpacity, Alert } from "react-native";
-import { HouseLine } from "phosphor-react-native";
+import { View, ScrollView, Alert, Pressable } from "react-native";
+import { HouseLine, Trash } from "phosphor-react-native";
 
 import { Header } from "../../components/Header";
 import { HistoryCard, HistoryProps } from "../../components/HistoryCard";
@@ -14,6 +14,8 @@ import Animated, {
   SlideInRight,
   SlideOutRight,
 } from "react-native-reanimated";
+import { Swipeable } from "react-native-gesture-handler";
+import { THEME } from "../../styles/theme";
 
 export function History() {
   const [isLoading, setIsLoading] = useState(true);
@@ -71,9 +73,17 @@ export function History() {
             exiting={SlideOutRight}
             key={item.id}
           >
-            <TouchableOpacity onPress={() => handleRemove(item.id)}>
+            <Swipeable
+              overshootLeft={false}
+              containerStyle={styles.swipeableContainer}
+              renderLeftActions={() => (
+                <Pressable style={styles.swipeableRemove}>
+                  <Trash size={32} color={THEME.COLORS.GREY_100} />
+                </Pressable>
+              )}
+            >
               <HistoryCard data={item} />
-            </TouchableOpacity>
+            </Swipeable>
           </Animated.View>
         ))}
       </ScrollView>
--- reactnative/2022/ignitequiz/src/screens/History/styles.ts
+++ reactnative/2022/ignitequiz/src/screens/History/styles.ts
@@ -1,6 +1,6 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
-import { THEME } from '../../styles/theme';
+import { THEME } from "../../styles/theme";
 
 export const styles = StyleSheet.create({
   container: {
@@ -9,5 +9,20 @@ export const styles = StyleSheet.create({
   },
   history: {
     padding: 32,
-  }
+  },
+  swipeableContainer: {
+    width: "100%",
+    backgroundColor: THEME.COLORS.DANGER_LIGHT,
+    borderRadius: 6,
+    height: 90,
+    marginBottom: 12,
+  },
+  swipeableRemove: {
+    width: 90,
+    height: 90,
+    borderRadius: 6,
+    backgroundColor: THEME.COLORS.DANGER_LIGHT,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
