e1cc59821bd2a4bea7948d2fe20d70dfa51f0f23
Vinícius Fraga <vinifragam@gmail.com>
2023-03-02 15:29:31 -0300

    Feedback visual - Animando o OverlayFeedback

--- reactnative/2022/ignitequiz/src/components/OverlayFeedback/index.tsx
+++ reactnative/2022/ignitequiz/src/components/OverlayFeedback/index.tsx
@@ -1,6 +1,14 @@
 import { BlurMask, Canvas, Rect } from "@shopify/react-native-skia";
+import { useEffect } from "react";
 import { useWindowDimensions } from "react-native";
-import Animated from "react-native-reanimated";
+import Animated, {
+  Easing,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withSequence,
+  withTiming,
+} from "react-native-reanimated";
 import { THEME } from "../../styles/theme";
 
 const STATUS = [
@@ -14,12 +22,28 @@ type Props = {
 };
 
 export function OverlayFeedback({ status = 0 }: Props) {
+  const opacity = useSharedValue(status);
   const color = STATUS[status];
 
   const { height, width } = useWindowDimensions();
 
+  const styleAnimated = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(opacity.value, [0, 1, 2], [0, 0.5, 0.5]),
+    };
+  });
+
+  useEffect(() => {
+    opacity.value = withSequence(
+      withTiming(1, { duration: 400, easing: Easing.bounce }),
+      withTiming(0)
+    );
+  }, [status]);
+
   return (
-    <Animated.View style={{ height, width, position: "absolute" }}>
+    <Animated.View
+      style={[{ height, width, position: "absolute" }, styleAnimated]}
+    >
       <Canvas style={{ flex: 1 }}>
         <Rect x={0} y={0} width={width} height={height} color={color}>
           <BlurMask blur={50} style="inner" />
