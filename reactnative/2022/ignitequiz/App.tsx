6f8a6b01e94716d5af20706980ca292127387658
Vinícius Fraga <vinifragam@gmail.com>
2023-03-02 12:29:29 -0300

    Microinterações - Instalando o React Native Gesture Handler

--- reactnative/2022/ignitequiz/App.tsx
+++ reactnative/2022/ignitequiz/App.tsx
@@ -1,26 +1,30 @@
-import { StatusBar } from 'react-native';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import "react-native-gesture-handler";
+import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import { Routes } from './src/routes';
-import { Loading } from './src/components/Loading';
+import { Routes } from "./src/routes";
+import { Loading } from "./src/components/Loading";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   if (!fontsLoaded) {
-    return (
-      <Loading />
-    );
+    return <Loading />;
   }
 
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <Routes />
-    </>
+    </GestureHandlerRootView>
   );
 }
