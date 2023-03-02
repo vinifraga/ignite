2420ee4d0ad67acb9fa5e74e96f5e384a1853e72
Vinícius Fraga <vinifragam@gmail.com>
2023-03-02 13:23:54 -0300

    Microinterações - Detectando Gestos

--- reactnative/2022/ignitequiz/src/screens/Quiz/index.tsx
+++ reactnative/2022/ignitequiz/src/screens/Quiz/index.tsx
@@ -25,6 +25,7 @@ import { ConfirmButton } from "../../components/ConfirmButton";
 import { OutlineButton } from "../../components/OutlineButton";
 import { ProgressBar } from "../../components/ProgressBar";
 import { THEME } from "../../styles/theme";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
 
 interface Params {
   id: string;
@@ -43,6 +44,7 @@ export function Quiz() {
 
   const shake = useSharedValue(0);
   const scrollY = useSharedValue(0);
+  const cardPosition = useSharedValue(0);
 
   const { navigate } = useNavigation();
 
@@ -166,6 +168,12 @@ export function Quiz() {
     };
   });
 
+  const onPan = Gesture.Pan()
+    .onUpdate((event) => (cardPosition.value = event.translationX))
+    .onEnd(() => {
+      cardPosition.value = withTiming(0);
+    });
+
   useEffect(() => {
     const quizSelected = QUIZ.filter((item) => item.id === id)[0];
     setQuiz(quizSelected);
@@ -200,14 +208,16 @@ export function Quiz() {
           />
         </Animated.View>
 
-        <Animated.View style={shakeStyleAnimated}>
-          <Question
-            key={quiz.questions[currentQuestion].title}
-            question={quiz.questions[currentQuestion]}
-            alternativeSelected={alternativeSelected}
-            setAlternativeSelected={setAlternativeSelected}
-          />
-        </Animated.View>
+        <GestureDetector gesture={onPan}>
+          <Animated.View style={shakeStyleAnimated}>
+            <Question
+              key={quiz.questions[currentQuestion].title}
+              question={quiz.questions[currentQuestion]}
+              alternativeSelected={alternativeSelected}
+              setAlternativeSelected={setAlternativeSelected}
+            />
+          </Animated.View>
+        </GestureDetector>
 
         <View style={styles.footer}>
           <OutlineButton title="Parar" onPress={handleStop} />
