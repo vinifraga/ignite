eccdabbf65e906dde8d19a957c6b3c88d1b77369
Vinícius Fraga <vinifragam@gmail.com>
2023-03-02 20:46:48 -0300

    Feedback táctil e sonoro - Prevenindo voltar com gesto no iOS

--- reactnative/2022/ignitequiz/src/routes/app.routes.tsx
+++ reactnative/2022/ignitequiz/src/routes/app.routes.tsx
@@ -1,35 +1,29 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { Home } from '../screens/Home';
-import { Quiz } from '../screens/Quiz';
-import { Finish } from '../screens/Finish';
-import { History } from '../screens/History';
+import { Home } from "../screens/Home";
+import { Quiz } from "../screens/Quiz";
+import { Finish } from "../screens/Finish";
+import { History } from "../screens/History";
 
-const { Navigator, Screen } = createNativeStackNavigator();
+const { Navigator, Screen, Group } = createNativeStackNavigator();
 
 export function AppRoutes() {
   return (
     <Navigator
       screenOptions={{
-        headerShown: false
+        headerShown: false,
       }}
     >
-      <Screen
-        name="home"
-        component={Home}
-      />
-      <Screen
-        name="quiz"
-        component={Quiz}
-      />
-      <Screen
-        name="history"
-        component={History}
-      />
-      <Screen
-        name="finish"
-        component={Finish}
-      />
+      <Screen name="home" component={Home} />
+      <Screen name="history" component={History} />
+      <Group
+        screenOptions={{
+          gestureEnabled: false,
+        }}
+      >
+        <Screen name="quiz" component={Quiz} />
+        <Screen name="finish" component={Finish} />
+      </Group>
     </Navigator>
-  )
+  );
 }
