c943aaa55ee66fb06f10439543d27c6ff5994f72
Vinícius Fraga <vinifragam@gmail.com>
2023-03-02 12:04:05 -0300

    Animações - Layout Animations

--- reactnative/2022/ignitequiz/src/screens/History/index.tsx
+++ reactnative/2022/ignitequiz/src/screens/History/index.tsx
@@ -1,14 +1,19 @@
-import { useEffect, useState } from 'react';
-import { useNavigation } from '@react-navigation/native';
-import { View, ScrollView, TouchableOpacity, Alert } from 'react-native';
-import { HouseLine } from 'phosphor-react-native';
+import { useEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { View, ScrollView, TouchableOpacity, Alert } from "react-native";
+import { HouseLine } from "phosphor-react-native";
 
-import { Header } from '../../components/Header';
-import { HistoryCard, HistoryProps } from '../../components/HistoryCard';
+import { Header } from "../../components/Header";
+import { HistoryCard, HistoryProps } from "../../components/HistoryCard";
 
-import { styles } from './styles';
-import { historyGetAll, historyRemove } from '../../storage/quizHistoryStorage';
-import { Loading } from '../../components/Loading';
+import { styles } from "./styles";
+import { historyGetAll, historyRemove } from "../../storage/quizHistoryStorage";
+import { Loading } from "../../components/Loading";
+import Animated, {
+  Layout,
+  SlideInRight,
+  SlideOutRight,
+} from "react-native-reanimated";
 
 export function History() {
   const [isLoading, setIsLoading] = useState(true);
@@ -29,17 +34,13 @@ export function History() {
   }
 
   function handleRemove(id: string) {
-    Alert.alert(
-      'Remover',
-      'Deseja remover esse registro?',
-      [
-        {
-          text: 'Sim', onPress: () => remove(id)
-        },
-        { text: 'Não', style: 'cancel' }
-      ]
-    );
-
+    Alert.alert("Remover", "Deseja remover esse registro?", [
+      {
+        text: "Sim",
+        onPress: () => remove(id),
+      },
+      { text: "Não", style: "cancel" },
+    ]);
   }
 
   useEffect(() => {
@@ -47,14 +48,14 @@ export function History() {
   }, []);
 
   if (isLoading) {
-    return <Loading />
+    return <Loading />;
   }
 
   return (
     <View style={styles.container}>
       <Header
         title="Histórico"
-        subtitle={`Seu histórico de estudos${'\n'}realizados`}
+        subtitle={`Seu histórico de estudos${"\n"}realizados`}
         icon={HouseLine}
         onPress={goBack}
       />
@@ -63,16 +64,18 @@ export function History() {
         contentContainerStyle={styles.history}
         showsVerticalScrollIndicator={false}
       >
-        {
-          history.map((item) => (
-            <TouchableOpacity
-              key={item.id}
-              onPress={() => handleRemove(item.id)}
-            >
+        {history.map((item) => (
+          <Animated.View
+            layout={Layout.springify()}
+            entering={SlideInRight}
+            exiting={SlideOutRight}
+            key={item.id}
+          >
+            <TouchableOpacity onPress={() => handleRemove(item.id)}>
               <HistoryCard data={item} />
             </TouchableOpacity>
-          ))
-        }
+          </Animated.View>
+        ))}
       </ScrollView>
     </View>
   );
