9aa82a474cf9d4ab9b99c48084d53cfb6072f15c
Vinícius Fraga <vinifragam@gmail.com>
2023-03-02 14:14:59 -0300

    Microinterações - Executando JS a partir da animação

--- reactnative/2022/ignitequiz/src/screens/Quiz/index.tsx
+++ reactnative/2022/ignitequiz/src/screens/Quiz/index.tsx
@@ -4,6 +4,7 @@ import Animated, {
   Easing,
   Extrapolate,
   interpolate,
+  runOnJS,
   useAnimatedScrollHandler,
   useAnimatedStyle,
   useSharedValue,
@@ -34,6 +35,7 @@ interface Params {
 type QuizProps = typeof QUIZ[0];
 
 const CARD_INCLINATION = 10;
+const CARD_SKIP_AREA = -200;
 
 export function Quiz() {
   const [points, setPoints] = useState(0);
@@ -176,7 +178,11 @@ export function Quiz() {
 
       if (moveToLeft) cardPosition.value = event.translationX;
     })
-    .onEnd(() => {
+    .onEnd((event) => {
+      if (event.translationX < CARD_SKIP_AREA) {
+        runOnJS(handleSkipConfirm)();
+      }
+
       cardPosition.value = withTiming(0);
     });
 
