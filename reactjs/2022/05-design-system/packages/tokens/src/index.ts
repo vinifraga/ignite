c91b94067a6ade05c63926b5008f8a97c8784789
Vini Fraga <vinifragam@gmail.com>
2023-05-22 17:41:06 -0300

    A base de um Design System - Configurando tokens

--- reactjs/2022/05-design-system/packages/tokens/src/index.ts
+++ reactjs/2022/05-design-system/packages/tokens/src/index.ts
@@ -1,1 +1,7 @@
 export * from './colors'
+export * from './font-sizes'
+export * from './font-weights'
+export * from './fonts'
+export * from './line-heights'
+export * from './radii'
+export * from './space'
