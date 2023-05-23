9efaff9a68612732b21e6f4b9e0cb7e30b9b9b8c
Vini Fraga <vinifragam@gmail.com>
2023-05-23 16:41:46 -0300

    Design System em React - Configuração do Stitches

---
+++ reactjs/2022/05-design-system/packages/react/src/styles/index.ts
@@ -0,0 +1,37 @@
+import {
+  colors,
+  fontSizes,
+  fontWeights,
+  fonts,
+  lineHeights,
+  radii,
+  space,
+} from '@ignite-ui/tokens'
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  getCssText,
+  keyframes,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space',
+  },
+
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
+  },
+})
