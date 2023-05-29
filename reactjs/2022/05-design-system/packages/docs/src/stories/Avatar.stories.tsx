fd3eda8085e595808c7346645efcdfb29c97a8ad
Vini Fraga <vinifragam@gmail.com>
2023-05-29 20:56:21 -0300

    Componentes do Design System - Componente: Avatar

---
+++ reactjs/2022/05-design-system/packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/vinifraga.png',
+    alt: 'Vinícius Fraga',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
---
+++ reactjs/2022/05-design-system/packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,18 @@
+import { User } from 'phosphor-react'
+
+import { AvatarContainer, AvatarFallback, AvatarImage } from './styles'
+import { ComponentProps } from 'react'
+
+export interface AvatarProps extends ComponentProps<typeof AvatarImage> {}
+
+export function Avatar(props: AvatarProps) {
+  return (
+    <AvatarContainer>
+      <AvatarImage {...props} />
+
+      <AvatarFallback delayMs={600}>
+        <User />
+      </AvatarFallback>
+    </AvatarContainer>
+  )
+}
