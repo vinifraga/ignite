1079dd9eab82455cc35317fc9cbf84c31c4947c1
Vini Fraga <vinifragam@gmail.com>
2023-05-30 11:48:48 -0300

    Melhorias no Storybook - Log de ações no componente

--- reactjs/2022/05-design-system/packages/docs/src/stories/Button.stories.tsx
+++ reactjs/2022/05-design-system/packages/docs/src/stories/Button.stories.tsx
@@ -8,6 +8,11 @@ export default {
   args: {
     children: 'Send',
   },
+  argTypes: {
+    onClick: {
+      action: 'click',
+    },
+  },
 } as Meta<ButtonProps>
 
 export const Primary: StoryObj<ButtonProps> = {}
