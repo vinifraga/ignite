ac48845b119c91b6a6281bb6c93c504109069f54
Vini Fraga <vinifragam@gmail.com>
2023-05-30 14:10:42 -0300

    Melhorias no Storybook - Publicando Storybook

--- reactjs/2022/05-design-system/packages/docs/.storybook/main.js
+++ reactjs/2022/05-design-system/packages/docs/.storybook/main.js
@@ -14,5 +14,12 @@ const config = {
   docs: {
     autodocs: true,
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/05-design-system/'
+    }
+
+    return config
+  }
 };
 export default config;
