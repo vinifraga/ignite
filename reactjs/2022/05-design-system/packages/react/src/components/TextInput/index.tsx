c16b87ac43c43349f5b15a760d1192a5b05a6b62
Vini Fraga <vinifragam@gmail.com>
2023-05-29 22:51:52 -0300

    Componentes do Design System - Componente: TextInput

---
+++ reactjs/2022/05-design-system/packages/react/src/components/TextInput/index.tsx
@@ -0,0 +1,16 @@
+import { ComponentProps } from 'react'
+import { Input, Prefix, TextInputContainer } from './styles'
+
+export interface TextInputProps extends ComponentProps<typeof Input> {
+  prefix?: string
+}
+
+export function TextInput({ prefix, ...props }: TextInputProps) {
+  return (
+    <TextInputContainer>
+      {!!prefix && <Prefix>{prefix}</Prefix>}
+
+      <Input {...props} />
+    </TextInputContainer>
+  )
+}
