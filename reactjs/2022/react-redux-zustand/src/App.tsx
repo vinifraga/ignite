080cbcf4f8a2f5f8c2223f0f8296a888816b292a
Vini Fraga <vinifragam@gmail.com>
2023-06-19 15:47:18 -0300

    Estrutura base da UI

--- reactjs/2022/react-redux-zustand/src/App.tsx
+++ reactjs/2022/react-redux-zustand/src/App.tsx
@@ -1,15 +1,14 @@
+import './styles/global.css';
+
 import { Provider as ReduxProvider } from "react-redux";
 
 import { store } from "./store";
-
-import { AddTodo } from "./components/AddTodos";
-import { TodoList } from "./components/TodoList";
+import { Player } from "./pages/Player";
 
 export function App() {
   return (
     <ReduxProvider store={store}>
-      <TodoList />
-      <AddTodo />
+      <Player />
     </ReduxProvider>
   )
 }
