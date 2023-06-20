7cf6e99e6e1c810c98a79ac043b7a6ce340e71be
Vini Fraga <vinifragam@gmail.com>
2023-06-20 20:15:59 -0300

    Autoplay nos vídeos

--- reactjs/2022/react-redux-zustand/src/components/Video.tsx
+++ reactjs/2022/react-redux-zustand/src/components/Video.tsx
@@ -1,8 +1,12 @@
 import ReactPlayer from "react-player";
+import { useDispatch } from "react-redux";
 
 import { useAppSelector } from "../store";
+import { next } from "../store/slices/player";
 
 export function Video() {
+  const dispatch = useDispatch()
+
   const lesson = useAppSelector(state => {
     const { currentLessonIndex, currentModuleIndex } = state.player
 
@@ -11,9 +15,20 @@ export function Video() {
     return currentLesson
   })
 
+  function handlePlayNext() {
+    dispatch(next())
+  }
+
   return (
     <div className="w-full bg-zinc-950 aspect-video">
-      <ReactPlayer width="100%" height="100%" controls url={`https://www.youtube.com/watch?v=${lesson.id}`} />
+      <ReactPlayer
+        width="100%"
+        height="100%"
+        controls
+        url={`https://www.youtube.com/watch?v=${lesson.id}`}
+        onEnded={handlePlayNext}
+        playing
+      />
     </div>
   )
 }
