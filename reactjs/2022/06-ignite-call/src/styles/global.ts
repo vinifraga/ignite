3912f7bfec762659d0b48aecebc4de4de8fd5f28
Vini Fraga <vinifragam@gmail.com>
2023-07-06 14:35:43 -0300

    Setup & estrutura - Configuração do ESLint

--- reactjs/2022/06-ignite-call/src/styles/global.ts
+++ reactjs/2022/06-ignite-call/src/styles/global.ts
@@ -1,4 +1,4 @@
-import { globalCss } from "@ignite-ui/react";
+import { globalCss } from '@ignite-ui/react'
 
 export const globalStyles = globalCss({
   '*': {
@@ -11,5 +11,5 @@ export const globalStyles = globalCss({
     backgroundColor: '$gray900',
     color: '$gray100',
     '-webkit-font-smoothing': 'antialiased',
-  }
+  },
 })
