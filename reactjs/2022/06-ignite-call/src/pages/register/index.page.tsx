b8df2fa0aa5a9d0e8b4fae4703db1df438b02367
Vini Fraga <vinifragam@gmail.com>
2023-07-06 18:39:28 -0300

    Cadastro de usuário - Persistindo usuário no Prisma

--- reactjs/2022/06-ignite-call/src/pages/register/index.page.tsx
+++ reactjs/2022/06-ignite-call/src/pages/register/index.page.tsx
@@ -6,6 +6,7 @@ import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import { api } from '@/lib/axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -41,7 +42,14 @@ export default function Register() {
   }, [router.query.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
