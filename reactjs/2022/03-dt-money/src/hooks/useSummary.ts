ee5c11b5d7c7c9af673e47b4f8d5c95bd851c286
Vini Fraga <vinifragam@gmail.com>
2023-07-06 14:14:07 -0300

    fix DTmoney useSummary

--- reactjs/2022/03-dt-money/src/hooks/useSummary.ts
+++ reactjs/2022/03-dt-money/src/hooks/useSummary.ts
@@ -10,7 +10,7 @@ export function useSummary() {
   )
 
   const summary = useMemo(() => {
-    transactions.reduce(
+    return transactions.reduce(
       (acc, transaction) => {
         if (transaction.type === 'income') {
           acc.income += transaction.price
