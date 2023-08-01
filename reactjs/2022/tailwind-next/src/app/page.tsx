ecfc316ef74d6dfd742e2dc1786eb300f6dc70d2
Vini Fraga <vinifragam@gmail.com>
2023-08-01 16:12:38 -0300

    Fundamentos do Tailwind- Criando Projeto com Tailwind

---
+++ reactjs/2022/tailwind-next/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="h-screen p-4 bg-slate-900 text-slate-100">
+    <h1 className="font-bold text-5xl">Hello Tailwind</h1>
+    <h3>Hello Tailwind</h3>
+    <p>Hello Tailwind</p>
+    </div>
+  )
+}
---
+++ reactjs/2022/tailwind-next/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        diego: '#8257e6'
+      }
+    },
+  },
+  plugins: [],
+}
