1447acaee537c3196a6ca63817ed25113759813d
Vini Fraga <vinifragam@gmail.com>
2023-08-14 17:31:28 -0300

    Formulário - Criando Abas com Radix Tabs

--- reactjs/2022/tailwind-next/src/app/page.tsx
+++ reactjs/2022/tailwind-next/src/app/page.tsx
@@ -1,3 +1,11 @@
+import { SettingsTabs } from '@/components/SettingsTabs'
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+
+      <SettingsTabs />
+    </>
+  )
 }
