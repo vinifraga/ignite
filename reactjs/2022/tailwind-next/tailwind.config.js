098833423831d52de5d6f4475231fedec3a1e10b
Vini Fraga <vinifragam@gmail.com>
2023-08-14 18:01:13 -0300

    Formulário - Campos do Formulário

--- reactjs/2022/tailwind-next/tailwind.config.js
+++ reactjs/2022/tailwind-next/tailwind.config.js
@@ -6,12 +6,10 @@ module.exports = {
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
         profile: 'min-content 1fr min-content',
+        form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
       maxWidth: {
         app: '700px',
-      },
-      colors: {
-        diego: '#8257e6',
       },
     },
   },
