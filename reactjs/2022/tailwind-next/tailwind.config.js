300089d936e17ff666db55b5252c0909117f9bd0
Vini Fraga <vinifragam@gmail.com>
2023-08-16 18:06:41 -0300

    Formulário - Upload do Avatar

--- reactjs/2022/tailwind-next/tailwind.config.js
+++ reactjs/2022/tailwind-next/tailwind.config.js
@@ -8,6 +8,17 @@ module.exports = {
         profile: 'min-content 1fr min-content',
         form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
+
+      borderWidth: {
+        6: '6px',
+      },
+
+      colors: {
+        violet: {
+          25: '#fcfaff',
+        },
+      },
+
       maxWidth: {
         app: '700px',
       },
