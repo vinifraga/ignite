9d8f7cde54f7ffc70ec1faa6b3e46dc617088c97
Vini Fraga <vinifragam@gmail.com>
2023-12-08 20:13:16 -0300

    Estrutura da aplicação - Rotas de criação e listagem (Métodos HTTP)

--- node/2023/01-fundamentos-nodejs/src/server.js
+++ node/2023/01-fundamentos-nodejs/src/server.js
@@ -1,6 +1,16 @@
 import http from 'node:http'
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res.end('Listagem de usuários')
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return res.end('Criação de usuário')
+  }
+
   return res.end('Hello World')
 })
 
