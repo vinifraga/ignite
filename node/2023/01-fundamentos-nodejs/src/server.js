4a539a48f3d6a2143ed9a5b3a0497dad9350a694
Vini Fraga <vinifragam@gmail.com>
2023-12-11 17:20:51 -0300

    Rotas da aplicação - Separando rotas da aplicação

--- node/2023/01-fundamentos-nodejs/src/server.js
+++ node/2023/01-fundamentos-nodejs/src/server.js
@@ -1,35 +1,20 @@
 import http from 'node:http'
-import { randomUUID } from 'node:crypto'
 
 import { json } from './middlewares/json.js'
-import { Database } from './database.js'
+import { routes } from './routes.js'
 
-const database = new Database()
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
   
-  if (method === 'GET' && url === '/users') {
-    const users = database.select('users')
-    
-    return res
-      .end(JSON.stringify(users))
-  }
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
+  })
 
-  if (method === 'POST' && url === '/users') {
-    const { email, name } = req.body
-
-    const user = ({
-      id: randomUUID(),
-      name,
-      email
-    })
-
-    database.insert('users', user)
-
-    return res.writeHead(201).end()
+  if (route) {
+    return route.handler(req, res)
   }
 
   return res.writeHead(404).end()
