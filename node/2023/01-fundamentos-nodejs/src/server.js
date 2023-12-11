821863151938a171884d3a03f0469a5b4ad10987
Vinícius Fraga <vinifragam@gmail.com>
2023-12-10 23:49:45 -0300

    Estrutura da aplicação - Salvando usuários em memória (Headers)

--- node/2023/01-fundamentos-nodejs/src/server.js
+++ node/2023/01-fundamentos-nodejs/src/server.js
@@ -1,13 +1,23 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuários')
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com'
+    })
+
     return res.end('Criação de usuário')
   }
 
