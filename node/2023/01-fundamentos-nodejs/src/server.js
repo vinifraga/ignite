88e3631ae8b9c15327ac4084f2d8210679f6fb56
Vini Fraga <vinifragam@gmail.com>
2023-12-11 20:03:29 -0300

    Rotas da aplicação - Capturando query parameters

--- node/2023/01-fundamentos-nodejs/src/server.js
+++ node/2023/01-fundamentos-nodejs/src/server.js
@@ -2,6 +2,7 @@ import http from 'node:http'
 
 import { json } from './middlewares/json.js'
 import { routes } from './routes.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
 
 
 const server = http.createServer(async (req, res) => {
@@ -16,7 +17,10 @@ const server = http.createServer(async (req, res) => {
   if (route) {
     const routeParams = req.url.match(route.path)
 
-    req.params = { ...routeParams.groups }
+    const { query, ...params } = routeParams.groups
+
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
 
     return route.handler(req, res)
   }
--- node/2023/01-fundamentos-nodejs/src/utils/build-route-path.js
+++ node/2023/01-fundamentos-nodejs/src/utils/build-route-path.js
@@ -2,7 +2,7 @@ export function buildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-Z]+)/g
   const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
 
-  const pathRegex = new RegExp(`^${pathWithParams}`)
+  const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`)
 
   return pathRegex
 }
