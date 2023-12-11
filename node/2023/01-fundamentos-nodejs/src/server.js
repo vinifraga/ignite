28cdaa7b86947cda10a58e8782804aeddc2fef87
Vinícius Fraga <vinifragam@gmail.com>
2023-12-10 23:54:09 -0300

    Estrutura da aplicação - Conhecendo HTTP status codes

--- node/2023/01-fundamentos-nodejs/src/server.js
+++ node/2023/01-fundamentos-nodejs/src/server.js
@@ -18,10 +18,10 @@ const server = http.createServer((req, res) => {
       email: 'johndoe@example.com'
     })
 
-    return res.end('Criação de usuário')
+    return res.writeHead(201).end()
   }
 
-  return res.end('Hello World')
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
