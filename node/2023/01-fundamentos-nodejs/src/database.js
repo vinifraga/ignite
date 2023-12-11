bd41f5630ac97572b989774af9b920867385f62c
Vini Fraga <vinifragam@gmail.com>
2023-12-11 19:37:39 -0300

    Rotas da aplicação - Atualização de registros

--- node/2023/01-fundamentos-nodejs/src/database.js
+++ node/2023/01-fundamentos-nodejs/src/database.js
@@ -37,8 +37,17 @@ export class Database {
     return data
   }
 
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    if (rowIndex > -1) {
+      this.#database[table][rowIndex] = { id, ...data }
+      this.#persist()
+    }
+  }
+
   delete(table, id) {
-    const rowIndex =this.#database[table].findIndex(row => row.id === id)
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
 
     if (rowIndex > -1) {
       this.#database[table].splice(rowIndex, 1)
--- node/2023/01-fundamentos-nodejs/src/routes.js
+++ node/2023/01-fundamentos-nodejs/src/routes.js
@@ -33,6 +33,21 @@ export const routes = [
     }
   },
   {
+    method: 'PUT',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { name, email } = req.body
+
+      database.update('users', id, {
+        name,
+        email
+      })
+
+      return res.writeHead(204).end()
+    }
+  },
+  {
     method: 'DELETE',
     path: buildRoutePath('/users/:id'),
     handler: (req, res) => {
