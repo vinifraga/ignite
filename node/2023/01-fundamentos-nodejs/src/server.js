dfd7f682765ee3b2922cf9aa606a4771b83c9439
Vinícius Fraga <vinifragam@gmail.com>
2023-12-11 01:17:43 -0300

    Streams no Node.js - Criando middleware de JSON

--- node/2023/01-fundamentos-nodejs/src/server.js
+++ node/2023/01-fundamentos-nodejs/src/server.js
@@ -1,25 +1,16 @@
 import http from 'node:http'
 
+import { json } from './middlewares/json.js'
+
 const users = []
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
-  const buffers = []
-
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    req.body = null
-  }
+  await json(req, res)
   
   if (method === 'GET' && url === '/users') {
     return res
-      .setHeader('Content-type', 'application/json')
       .end(JSON.stringify(users))
   }
 
