819cd2354bd10db230ed4401f2fb74078f12cc5a
Vini Fraga <vinifragam@gmail.com>
2023-12-15 12:18:12 -0300

    Banco de dados - Tratando env com Zod

--- node/2023/02-api-rest-nodejs/src/database.ts
+++ node/2023/02-api-rest-nodejs/src/database.ts
@@ -1,15 +1,10 @@
-import 'dotenv/config'
-
 import { knex as setupKnex, Knex } from 'knex'
-
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL env not found.')
-}
+import { env } from './env'
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: process.env.DATABASE_URL,
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
@@ -17,5 +12,7 @@ export const config: Knex.Config = {
     directory: './db/migrations',
   },
 }
+
+console.log(env)
 
 export const knex = setupKnex(config)
