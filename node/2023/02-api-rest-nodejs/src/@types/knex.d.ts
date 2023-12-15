b94ad32444022b1fffb13db92f7d2f5223e9142b
Vini Fraga <vinifragam@gmail.com>
2023-12-15 16:13:21 -0300

    Implementando as rotas - Tipagem no Knex

---
+++ node/2023/02-api-rest-nodejs/src/@types/knex.d.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    transactions: {
+      id: string
+      title: string
+      amount: number
+      created_at: string
+      session_id?: string
+    }
+  }
+}
