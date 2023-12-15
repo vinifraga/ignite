3ea07ae0c100623346bf85f40cfc58f83a73854a
Vini Fraga <vinifragam@gmail.com>
2023-12-15 16:32:29 -0300

    Implementando as rotas - Utilizando cookies no Fastify

--- node/2023/02-api-rest-nodejs/src/routes/transactions.ts
+++ node/2023/02-api-rest-nodejs/src/routes/transactions.ts
@@ -40,10 +40,22 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
     const { amount, title, type } = createTransactionBodySchema.parse(req.body)
 
+    let sessionId = req.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = crypto.randomUUID()
+
+      res.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
     await knex('transactions').insert({
       id: crypto.randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     return res.status(201).send()
--- node/2023/02-api-rest-nodejs/src/server.ts
+++ node/2023/02-api-rest-nodejs/src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify'
+import fastifyCookie from '@fastify/cookie'
+
 import { env } from './env'
 import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
+app.register(fastifyCookie)
 app.register(transactionsRoutes, {
   prefix: 'transactions',
 })
