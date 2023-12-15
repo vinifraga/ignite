32bba254433d44e05bbadd0a79607740260da5e1
Vini Fraga <vinifragam@gmail.com>
2023-12-15 15:02:22 -0300

    Implementando as rotas - Criação de transações

--- node/2023/02-api-rest-nodejs/src/routes/transactions.ts
+++ node/2023/02-api-rest-nodejs/src/routes/transactions.ts
@@ -1,12 +1,23 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import { z } from 'zod'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transactions = await knex('transactions')
-      .where('amount', 1000)
-      .select('*')
+  app.post('/', async (req, res) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transactions
+    const { amount, title, type } = createTransactionBodySchema.parse(req.body)
+
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return res.status(201).send()
   })
 }
